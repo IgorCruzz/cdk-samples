d42d16718128d781d913a7e17e9f6fed9c6f5e90
igorcruzz <igorcruz.dev@gmail.com>
2025-04-10 13:25:07 -0300

    feat: add api gateway stack

--- lib/notifier-stack.ts
+++ lib/notifier-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Architecture, LoggingFormat, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
+import { RestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class NotifierStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -31,5 +32,84 @@ export class NotifierStack extends cdk.Stack {
 
         cdk.Tags.of(notifierValidationFunction).add('Project', 'Notifier');
         cdk.Tags.of(notifierValidationFunction).add('Environment', notifierValidationFunction.stack.stackName);
+
+        const notifierApi = new RestApi(this, 'notifierApi', {
+            restApiName: 'Notifier API',
+            description: 'API for the Notifier service',
+            endpointConfiguration: {
+                types: [cdk.aws_apigateway.EndpointType.REGIONAL],
+            },
+            cloudWatchRole: true,
+            deployOptions: {
+                loggingLevel: cdk.aws_apigateway.MethodLoggingLevel.INFO,
+                metricsEnabled: true,
+                tracingEnabled: true,
+                stageName: 'prod',
+            },
+            defaultCorsPreflightOptions: {
+                allowOrigins: cdk.aws_apigateway.Cors.ALL_ORIGINS,
+                allowMethods: ['OPTIONS', 'POST'],
+                allowHeaders: ['Content-Type', 'X-Amz-Date', 'Authorization', 'X-Api-Key', 'X-Amz-Security-Token'],
+            },
+        });
+
+        cdk.Tags.of(notifierApi).add('Project', 'Notifier');
+        cdk.Tags.of(notifierApi).add('Environment', notifierApi.stack.stackName);
+
+        const notifierResource = notifierApi.root.addResource('notifications');
+
+        const notifierIntegration = new cdk.aws_apigateway.LambdaIntegration(notifierValidationFunction);
+
+        const notificationsPostRequestModel = new cdk.aws_apigateway.Model(this, 'notificationsPostRequestModel', {
+            restApi: notifierApi,
+            contentType: 'application/json',
+            modelName: 'notificationsPostRequestModel',
+            description: 'Model for notifications POST request',
+            schema: {
+                type: cdk.aws_apigateway.JsonSchemaType.OBJECT,
+                properties: {
+                    notifications: {
+                        type: cdk.aws_apigateway.JsonSchemaType.ARRAY,
+                        minItems: 1,
+                        maxItems: 15,
+                        items: {
+                            type: cdk.aws_apigateway.JsonSchemaType.OBJECT,
+                            properties: {
+                                priority: {
+                                    type: cdk.aws_apigateway.JsonSchemaType.STRING,
+                                    enum: ['HIGH', 'MEDIUM', 'LOW'],
+                                },
+                                title: { type: cdk.aws_apigateway.JsonSchemaType.STRING, minLength: 1, maxLength: 100 },
+                                userId: { type: cdk.aws_apigateway.JsonSchemaType.STRING, format: 'uuid' },
+                                message: {
+                                    type: cdk.aws_apigateway.JsonSchemaType.STRING,
+                                    minLength: 1,
+                                    maxLength: 500,
+                                },
+                            },
+                            required: ['priority', 'message', 'title', 'userId'],
+                        },
+                    },
+                },
+                required: ['notifications'],
+            },
+        });
+
+        const notificationsPostRequestValidator = new cdk.aws_apigateway.RequestValidator(
+            this,
+            'notificationsPostRequestValidator',
+            {
+                restApi: notifierApi,
+                requestValidatorName: 'notificationsPostRequestValidator',
+                validateRequestBody: true,
+            },
+        );
+
+        notifierResource.addMethod('POST', notifierIntegration, {
+            requestModels: {
+                'application/json': notificationsPostRequestModel,
+            },
+            requestValidator: notificationsPostRequestValidator,
+        });
     }
 }
