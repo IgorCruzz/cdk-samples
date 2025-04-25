fb1fa8f2d0045c5c08299ffb12632f2c513ef677
igorcruzz <igorcruz.dev@gmail.com>
2025-04-24 23:02:49 -0300

    feat: create export index import for services

---
+++ lambda/services/index.ts
@@ -0,0 +1,3 @@
+export { INotifierValidationService, NotifierValidationService } from '../services/notifier-validation.service';
+export { INotifierProcessDLQService, NotifierProcessDLQService } from '../services/notify-process-dlq.service';
+export { INotifierProcessService, NotifierProcessService } from '../services/notifier-process.service';
