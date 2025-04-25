4d845850ebcacccbe2febd230566fc10b7e541d3
igorcruzz <igorcruz.dev@gmail.com>
2025-04-24 23:02:24 -0300

    feat: create export index import for controllers

---
+++ lambda/controllers/index.ts
@@ -0,0 +1,3 @@
+export { NotifierProcessController } from './notifier-process.controller';
+export { NotifierValidationController } from './notifier-validation.controller';
+export { NotifierProcessDLQController } from './notify-process-dlq.controller';
