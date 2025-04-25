6303b38650d344c27ac0a69f7e7700079c3fa24c
igorcruzz <igorcruz.dev@gmail.com>
2025-04-25 20:25:02 -0300

    refactor: simplify handler

--- rest-api/lambda/handlers/index.ts
+++ rest-api/lambda/handlers/index.ts
@@ -1,27 +1,8 @@
-import { NotifierProcessController, NotifierProcessDLQController, NotifierValidationController } from '../controllers';
-import { NotifierProcessService, NotifierProcessDLQService, NotifierValidationService } from '../services';
-import { NotifierRepository } from '../repositories';
-import { SnsAdapter } from '../adapters';
 import { APIGatewayProxyEvent, SQSEvent } from 'aws-lambda';
+import { notifierProcessController, notifierProcessDLQController, notifierValidationController } from '../controllers';
 
-export const notifierProcessHandler = (event: SQSEvent) => {
-    const repository = new NotifierRepository();
-    const service = new NotifierProcessService(repository);
-    const controller = new NotifierProcessController(service);
+export const notifierProcessHandler = async (event: SQSEvent) => await notifierProcessController(event);
 
-    return controller.handler(event);
-};
+export const notifierProcessDLQHandler = async (event: SQSEvent) => await notifierProcessDLQController(event);
 
-export const notifierProcessDLQHandler = (event: SQSEvent) => {
-    const service = new NotifierProcessDLQService();
-    const controller = new NotifierProcessDLQController(service);
-
-    return controller.handler(event);
-};
-export const notifierValidationHandler = (event: APIGatewayProxyEvent) => {
-    const adapter = new SnsAdapter();
-    const service = new NotifierValidationService(adapter);
-    const controller = new NotifierValidationController(service);
-
-    return controller.handler(event);
-};
+export const notifierValidationHandler = (event: APIGatewayProxyEvent) => notifierValidationController(event);
