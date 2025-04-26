25d3a27861209208733d79217cc50ffd28c0d580
igorcruzz <igorcruz.dev@gmail.com>
2025-04-26 14:43:28 -0300

    test: notifierProcessService should be able to call sendWhatsAppMessage

--- rest-api/lambda/test/services/notifier-process.test.ts
+++ rest-api/lambda/test/services/notifier-process.test.ts
@@ -1,10 +1,12 @@
 import { notifierProcessService } from '../../services';
 import { notifierTable } from '../../repositories';
 import { SQSRecord } from 'aws-lambda';
+import * as twilio from '../../shared/twilio';
 
 describe('notifierProcessService', () => {
     beforeEach(() => {
         jest.spyOn(notifierTable, 'putItem').mockResolvedValue();
+        jest.spyOn(twilio, 'sendWhatsAppMessage').mockImplementation();
     });
 
     it('should be defined', async () => {
@@ -43,7 +45,53 @@ describe('notifierProcessService', () => {
             },
         ];
 
+        const putItem = jest.spyOn(notifierTable, 'putItem');
+
         await notifierProcessService({ records });
+
+        expect(putItem).toHaveBeenCalled();
+    });
+
+    it('should be able to call sendWhatsAppMessage', async () => {
+        const records: SQSRecord[] = [
+            {
+                messageId: '059f36b4-87a3-44ab-83d2-661975830a7d',
+                receiptHandle: 'AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...',
+                body: JSON.stringify({
+                    message: 'message',
+                    priority: 'HIGH',
+                    title: 'title',
+                    userId: 'userId',
+                }),
+                attributes: {
+                    ApproximateReceiveCount: '1',
+                    SentTimestamp: '1545082649183',
+                    SenderId: 'AIDAIENQZJOLO23YVJ4VO',
+                    ApproximateFirstReceiveTimestamp: '1545082649185',
+                },
+                messageAttributes: {
+                    myAttribute: {
+                        stringValue: 'myValue',
+                        stringListValues: [],
+                        binaryListValues: [],
+                        dataType: 'String',
+                    },
+                },
+                md5OfBody: 'e4e68fb7bd0e697a0ae8f1bb342846b3',
+                eventSource: 'aws:sqs',
+                eventSourceARN: 'arn:aws:sqs:us-east-2:123456789012:my-queue',
+                awsRegion: 'us-east-2',
+            },
+        ];
+
+        const sendWhatsAppMessage = jest.spyOn(twilio, 'sendWhatsAppMessage');
+
+        await notifierProcessService({ records });
+
+        expect(sendWhatsAppMessage).toHaveBeenCalled();
+        expect(sendWhatsAppMessage).toHaveBeenCalledWith({
+            message: 'message',
+        });
     });
 
     it('should return empty batchItemFailures when all records are processed successfully', async () => {
