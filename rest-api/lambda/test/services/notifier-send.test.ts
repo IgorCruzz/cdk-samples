9c1660f173a97cfcaa4f956ac1b35191f3bb68cd
igorcruzz <igorcruz.dev@gmail.com>
2025-04-26 13:26:38 -0300

    test: notifierSendService should throw an error if publishMessage fails

--- rest-api/lambda/test/services/notifier-send.test.ts
+++ rest-api/lambda/test/services/notifier-send.test.ts
@@ -1,10 +1,10 @@
 import { notifierSendService } from '../../services';
-import * as foo from '../../shared/sns';
+import * as sns from '../../shared/sns';
 import { NotifyType } from '../../types';
 
 describe('notifierSendService', () => {
     beforeAll(() => {
-        jest.spyOn(foo, 'publishMessage').mockResolvedValue();
+        jest.spyOn(sns, 'publishMessage').mockResolvedValue();
     });
 
     it('should be defined', async () => {
@@ -21,10 +21,32 @@ describe('notifierSendService', () => {
             },
         ];
 
-        const publishMessage = jest.spyOn(foo, 'publishMessage');
+        const publishMessage = jest.spyOn(sns, 'publishMessage');
 
         await notifierSendService({ notifications });
 
         expect(publishMessage).toHaveBeenCalledWith({ notifications });
+    });
+
+    it('should throw an error if publishMessage fails', async () => {
+        const notifications: NotifyType[] = [
+            {
+                priority: 'HIGH',
+                userId: '28360287-9c42-4f14-971b-6e657bf030b5',
+                title: 'title',
+                message: 'message',
+            },
+        ];
+
+        jest.spyOn(sns, 'publishMessage').mockRejectedValue(new Error('Error sending notification'));
+
+        const service = await notifierSendService({ notifications });
+
+        expect(service).toEqual({
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Internal Server Error',
+            }),
+        });
     });
 });
