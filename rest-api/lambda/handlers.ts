a472bab202d65bcb8e2463f0d9e28fc84c417d30
igorcruzz <igorcruz.dev@gmail.com>
2025-04-26 13:44:25 -0300

    refactor: pass event to service

--- rest-api/lambda/handlers.ts
+++ rest-api/lambda/handlers.ts
@@ -2,12 +2,16 @@ import { APIGatewayProxyEvent, SQSEvent } from 'aws-lambda';
 import { notifierProcessDLQService, notifierProcessService, notifierSendService } from './services';
 import { NotifyType } from './types';
 
-export const notifierProcessHandler = async (event: SQSEvent) => await notifierProcessService(event);
+export const notifierProcessHandler = async (event: SQSEvent) => {
+    const { Records } = event;
+
+    return await notifierProcessService({ records: Records });
+};
 
 export const notifierProcessDLQHandler = async (event: SQSEvent) => await notifierProcessDLQService(event);
 
 export const notifierSendHandler = async (event: APIGatewayProxyEvent) => {
     const body: { notifications: NotifyType[] } = JSON.parse(event.body || '');
 
-    await notifierSendService({ notifications: body.notifications });
+    return await notifierSendService({ notifications: body.notifications });
 };
