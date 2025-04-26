dad78e9b7b306bcd7369d61d5b7a2aaf88da5850
igorcruzz <igorcruz.dev@gmail.com>
2025-04-26 12:05:06 -0300

    feat: create repository for table

---
+++ rest-api/lambda/repositories/index.ts
@@ -0,0 +1,1 @@
+export * as notifierTable from './notifierTable';
---
+++ rest-api/lambda/repositories/notifierTable.ts
@@ -0,0 +1,30 @@
+import { DynamoDBDocumentClient, PutCommand, PutCommandInput } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { NotifyType } from '../types';
+import { randomUUID } from 'node:crypto';
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const putItem = async (message: NotifyType) => {
+    const PK = randomUUID();
+
+    const params: PutCommandInput = {
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Item: {
+            PK: `USER#${message.userId}`,
+            SK: `NOTIFICATION#${PK}`,
+            UserId: message.userId,
+            Message: message.message,
+            Priority: message.priority,
+            Title: message.title,
+            Timestamp: new Date().toISOString(),
+            GSI1PK: message.priority,
+            GSI1SK: `USER#${message.userId}`,
+        },
+    };
+
+    const command = new PutCommand(params);
+
+    await docClient.send(command);
+};
