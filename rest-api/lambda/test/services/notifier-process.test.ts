70cb67834b093ef8bf49114c2433487b364af537
igorcruzz <igorcruz.dev@gmail.com>
2025-04-26 13:29:27 -0300

    test: notifierProcessService should be defined

---
+++ rest-api/lambda/test/services/notifier-process.test.ts
@@ -0,0 +1,7 @@
+import { notifierProcessService } from '../../services';
+
+describe('notifierProcessService', () => {
+    it('should be defined', async () => {
+        expect(notifierProcessService).toBeDefined();
+    });
+});
