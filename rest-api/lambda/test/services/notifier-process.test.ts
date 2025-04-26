f721011994ecfa0a6859685feee62d7839da715b
igorcruzz <igorcruz.dev@gmail.com>
2025-04-26 13:43:45 -0300

    test: notifierProcessService should return batchItemFailures when there is an error processing a record

--- rest-api/lambda/test/services/notifier-process.test.ts
+++ rest-api/lambda/test/services/notifier-process.test.ts
@@ -84,4 +84,49 @@ describe('notifierProcessService', () => {
             batchItemFailures: [],
         });
     });
+
+    it('should return batchItemFailures when there is an error processing a record', async () => {
+        jest.spyOn(notifierTable, 'putItem').mockRejectedValue(new Error('Error processing item'));
+
+        const records: SQSRecord[] = [
+            {
+                messageId: '059f36b4-87a3-44ab-83d2-661975830a7d',
+                receiptHandle: 'AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...',
+                body: JSON.stringify({
+                    message: 'message',
+                    priority: 'HIGH',
+                    title: 'title',
+                    userId: 'userId',
+                }),
+                attributes: {
+                    ApproximateReceiveCount: '1',
+                    SentTimestamp: '1545082649183',
+                    SenderId: 'AIDAIENQZJOLO23YVJ4VO',
+                    ApproximateFirstReceiveTimestamp: '1545082649185',
+                },
+                messageAttributes: {
+                    myAttribute: {
+                        stringValue: 'myValue',
+                        stringListValues: [],
+                        binaryListValues: [],
+                        dataType: 'String',
+                    },
+                },
+                md5OfBody: 'e4e68fb7bd0e697a0ae8f1bb342846b3',
+                eventSource: 'aws:sqs',
+                eventSourceARN: 'arn:aws:sqs:us-east-2:123456789012:my-queue',
+                awsRegion: 'us-east-2',
+            },
+        ];
+
+        const service = await notifierProcessService({ records });
+
+        expect(service).toEqual({
+            batchItemFailures: [
+                {
+                    itemIdentifier: '059f36b4-87a3-44ab-83d2-661975830a7d',
+                },
+            ],
+        });
+    });
 });
