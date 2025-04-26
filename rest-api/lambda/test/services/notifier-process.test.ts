6b18678688d175ed9883610b0233a62a2e9ed70b
igorcruzz <igorcruz.dev@gmail.com>
2025-04-26 12:16:32 -0300

    test: notifierProcessService should be able to call publishMessage

--- rest-api/lambda/test/services/notifier-process.test.ts
+++ rest-api/lambda/test/services/notifier-process.test.ts
@@ -1,7 +1,30 @@
 import { notifierProcessService } from '../../services';
+import { notifierTable } from '../../repositories';
+import { NotifyType } from '../../types';
+
+jest.mock('../../repositories', () => ({
+    notifierTable: {
+        putItem: jest.fn(),
+    },
+}));
 
 describe('notifierProcessService', () => {
     it('should be defined', async () => {
         expect(notifierProcessService).toBeDefined();
+    });
+
+    it('should be able to call putItem', async () => {
+        const notification: NotifyType = {
+            message: 'message',
+            priority: 'HIGH',
+            title: 'title',
+            userId: 'userId',
+        };
+
+        const notifierTableMock = jest.spyOn(notifierTable, 'putItem');
+
+        await notifierProcessService({ notification });
+
+        expect(notifierTableMock).toHaveBeenCalledWith(notification);
     });
 });
