9a420a96371512a56103cc5797e5cbd11527a9f1
igorcruzz <igorcruz.dev@gmail.com>
2025-05-03 22:12:58 -0300

    feat: create sendmail function

--- rest-api/lambda/shared/ses.ts
+++ rest-api/lambda/shared/ses.ts
@@ -1,3 +1,22 @@
-export const sendMail = async ({ message }: { message: string }) => {
-    return null;
+import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
+
+const client = new SESClient({});
+
+export const sendMail = async ({ message, subject }: { message: string; subject: string }) => {
+    const result = await client.send(
+        new SendEmailCommand({
+            Destination: {
+                ToAddresses: ['igorcruz.dev@gmail.com'],
+            },
+            Message: {
+                Subject: { Data: subject },
+                Body: {
+                    Text: { Data: message },
+                },
+            },
+            Source: 'igorcruz.dev@gmail.com',
+        }),
+    );
+
+    console.log('E-mail enviado! ID:', result.MessageId);
 };
