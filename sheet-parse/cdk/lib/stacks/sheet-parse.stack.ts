e0900693402f556f843c10133f7d42b83b3890a5
igorcruzz <igorcruz.dev@gmail.com>
2025-05-17 20:46:01 -0300

    feat: create stack

--- sheet-parse/cdk/lib/stacks/sheet-parse.stack.ts
+++ sheet-parse/cdk/lib/stacks/sheet-parse.stack.ts
@@ -1,12 +1,16 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { ApiConstruct, S3Construct } from "../constructs";
+import { ApiConstruct, S3Construct, LambdaConstruct } from "../constructs";
 
 export class SheetParseStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new ApiConstruct(this, "apiConstruct");
-    new S3Construct(this, "s3Construct");
+    const apiConstruct = new ApiConstruct(this, "apiConstruct");
+    const s3Construct = new S3Construct(this, "s3Construct");
+    new LambdaConstruct(this, "lambdaConstruct", {
+      apiConstruct,
+      s3Construct,
+    });
   }
 }
