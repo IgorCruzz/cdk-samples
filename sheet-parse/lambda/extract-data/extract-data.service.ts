2c265c11959a531fffa5c79a0ce88a8aae349bc7
igorcruzz <igorcruz.dev@gmail.com>
2025-05-24 00:30:44 -0300

    feat: implement ExtractDataService for processing S3 records

---
+++ sheet-parse/lambda/extract-data/extract-data.service.ts
@@ -0,0 +1,52 @@
+import { S3EventRecord } from "aws-lambda";
+import { S3Interface } from "../shared/s3";
+import { parse } from "csv-parse";
+import { DynamoDBInterface } from "../shared/dynamodb";
+
+export class ExtractDataService {
+  constructor(
+    private readonly s3: S3Interface,
+    private readonly table: DynamoDBInterface
+  ) {}
+
+  extract = async ({ s3Record }: { s3Record: S3EventRecord }) => {
+    const stream = await this.s3.getObject({
+      key: s3Record.s3.object.key,
+      Bucket: "sheet-parse-bucket",
+    });
+
+    const chunk = [];
+
+    for await (const customer of stream.pipe(
+      parse({
+        skipEmptyLines: true,
+        columns: true,
+        trim: true,
+      })
+    )) {
+      chunk.push({
+        firstName: customer["First Name"],
+        lastName: customer["Last Name"],
+        company: customer["Company"],
+        city: customer["City"],
+        country: customer["Country"],
+        phone1: customer["Phone 1"],
+        phone2: customer["Phone 2"],
+        email: customer["Email"],
+        website: customer["Website"],
+      });
+
+      if (chunk.length === 20) {
+        await this.table.putItem({ data: chunk });
+        chunk.length = 0;
+      }
+    }
+
+    if (chunk.length > 0) {
+      await this.table.putItem({ data: chunk });
+      chunk.length = 0;
+    }
+
+    return;
+  };
+}
