d011dbc1aec1c23b51dd70fa7a872fe6cb2f2c0d
igorcruzz <igorcruz.dev@gmail.com>
2025-05-24 00:30:20 -0300

    feat: add dynamodb service

---
+++ sheet-parse/lambda/shared/dynamodb.ts
@@ -0,0 +1,44 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  BatchWriteCommandInput,
+  BatchWriteCommand,
+} from "@aws-sdk/lib-dynamodb";
+import { CustomerType } from "../types/customer";
+import KSUID from "ksuid";
+
+const client = new DynamoDBClient({});
+
+export interface DynamoDBInterface {
+  putItem: ({ data }: { data: CustomerType[] }) => Promise<void>;
+}
+
+export class DynamoDB implements DynamoDBInterface {
+  async putItem({ data }: { data: CustomerType[] }): Promise<void> {
+    const customers = await Promise.all(
+      data.map(async (item) => {
+        const ID = (await KSUID.random()).string;
+
+        return {
+          PutRequest: {
+            Item: {
+              ...item,
+              PK: `CUSTOMER#${ID}`,
+              SK: `METADATA#${ID}`,
+              CreatedAt: new Date().toISOString(),
+            },
+          },
+        };
+      })
+    );
+
+    const params: BatchWriteCommandInput = {
+      RequestItems: {
+        [process.env.TABLE_NAME as string]: customers,
+      },
+    };
+
+    const command = new BatchWriteCommand(params);
+
+    await client.send(command);
+  }
+}
