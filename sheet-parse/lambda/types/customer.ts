229d19053d156412b0f1099cffe78b0da24de745
igorcruzz <igorcruz.dev@gmail.com>
2025-05-24 00:30:30 -0300

    feat: add CustomerType definition

---
+++ sheet-parse/lambda/types/customer.ts
@@ -0,0 +1,11 @@
+export type CustomerType = {
+  firstName: string;
+  lastName: string;
+  company: string;
+  city: string;
+  country: string;
+  phone1: string;
+  phone2: string;
+  email: string;
+  website: string;
+};
