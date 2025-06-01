1addc51b7aeeef34864e50d211f6a299f59fefcc
igorcruzz <igorcruz.dev@gmail.com>
2025-06-01 17:11:19 -0300

    fix: update import path for GlobalStack in global.ts

--- global/cdk/bin/global.ts
+++ global/cdk/bin/global.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { GlobalStack } from '../lib/global-stack';
+import { GlobalStack } from '../lib/stacks/global-stack';
 
 const app = new cdk.App();
 new GlobalStack(app, 'GlobalStack');
