205f8fd04e23cb38597369e6f4ed3d9ae019c43f
igorcruzz <igorcruz.dev@gmail.com>
2025-06-01 23:03:43 -0300

    Remove global package configuration files: package.json and tsconfig.json

--- global/cdk/bin/global.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { GlobalStack } from '../lib/stacks/global-stack';
-
-const app = new cdk.App();
-new GlobalStack(app, 'GlobalStack');
