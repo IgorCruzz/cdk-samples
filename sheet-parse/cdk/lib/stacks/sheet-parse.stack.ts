2232b05485a8616084bf4c3f571c72ebdd81517d
igorcruzz <igorcruz.dev@gmail.com>
2025-06-01 23:05:00 -0300

    refactor: remove ApiConstruct and its related logic from SheetParseStack

--- sheet-parse/cdk/lib/stacks/sheet-parse.stack.ts
+++ sheet-parse/cdk/lib/stacks/sheet-parse.stack.ts
@@ -1,29 +1,19 @@
 import { Stack, StackProps, Tags } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import {
-  ApiConstruct,
-  S3Construct,
-  LambdaConstruct,
-  DynamoDBConstruct,
-} from "../constructs";
+import { S3Construct, LambdaConstruct, DynamoDBConstruct } from "../constructs";
 
 export class SheetParseStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const apiConstruct = new ApiConstruct(this, "apiConstruct");
     const s3Construct = new S3Construct(this, "s3Construct");
 
     const dynamoDBConstruct = new DynamoDBConstruct(this, "dynamoConstrut");
 
     const lambdaConstruct = new LambdaConstruct(this, "lambdaConstruct", {
-      apiConstruct,
       s3Construct,
       dynamoDBConstruct,
     });
-
-    Tags.of(apiConstruct.sheetParseApi).add("Project", "sheet-parse");
-    Tags.of(apiConstruct.sheetParseApi).add("Name", "sheet-parse-api");
 
     Tags.of(s3Construct.bucket).add("Project", "sheet-parse");
     Tags.of(s3Construct.bucket).add("Name", "sheet-parse-bucket");
