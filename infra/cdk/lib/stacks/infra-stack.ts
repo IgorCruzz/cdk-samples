9a24aedbfe0e87615eda2bb8d856d71e7c7fc7bd
igorcruzz <igorcruz.dev@gmail.com>
2025-06-03 20:13:16 -0300

    feat: refactor InfraStack to integrate Route53Construct and ACMConstruct for API management

--- infra/cdk/lib/stacks/infra-stack.ts
+++ infra/cdk/lib/stacks/infra-stack.ts
@@ -1,11 +1,19 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { ApiConstruct } from "../constructs/api.construct";
+import { Route53Construct } from "../constructs/route53.construct";
+import { ACMConstruct } from "../constructs/acm.construct";
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new ApiConstruct(this, "XyzApi");
+    const route53 = new Route53Construct(this, "Route53Construct");
+    const acm = new ACMConstruct(this, "ACMConstruct", {
+      route53,
+    });
+    new ApiConstruct(this, "XyzApi", {
+      acm,
+    });
   }
 }
