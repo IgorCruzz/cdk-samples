95ff4e63507951509383782f8dae3d5aac570618
igorcruzz <igorcruz.dev@gmail.com>
2025-06-04 21:35:59 -0300

    fix: remove InfraStack and ensure correct imports for Route53Stack, AcmStack, and ApiStack in infra.ts

--- infra/cdk/bin/infra.ts
+++ infra/cdk/bin/infra.ts
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { InfraStack } from "../lib/stacks/infra-stack";
+import { Route53Stack } from "../lib/stacks/route53.stack";
+import { AcmStack } from "../lib/stacks/acm-stack";
+import { ApiStack } from "../lib/stacks/api-stack";
 
 const app = new cdk.App();
-new InfraStack(app, "InfraStack");
+
+new Route53Stack(app, "Route53Stack");
+
+new AcmStack(app, "AcmStack");
+
+new ApiStack(app, "ApiStack");
