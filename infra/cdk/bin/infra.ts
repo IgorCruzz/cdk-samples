60a1dc5199686c50b1cf0de33d24e6d7ecaf2a55
igorcruzz <igorcruz.dev@gmail.com>
2025-06-05 14:17:18 -0300

    fix: add environment configuration to AcmStack instantiation

--- infra/cdk/bin/infra.ts
+++ infra/cdk/bin/infra.ts
@@ -7,7 +7,10 @@ import { ApiStack } from "../lib/stacks/api-stack";
 const app = new cdk.App();
 
 new Route53Stack(app, "Route53Stack");
-
-new AcmStack(app, "AcmStack");
-
+new AcmStack(app, "AcmStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
 new ApiStack(app, "ApiStack");
