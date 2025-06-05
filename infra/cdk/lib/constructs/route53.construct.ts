33c5f6804214146f222a7a95be4c436447633ce8
igorcruzz <igorcruz.dev@gmail.com>
2025-06-04 21:36:11 -0300

    fix: refactor Route53Construct to streamline hosted zone creation and parameter storage

--- infra/cdk/lib/constructs/route53.construct.ts
+++ infra/cdk/lib/constructs/route53.construct.ts
@@ -1,18 +1,24 @@
 import { Construct } from "constructs";
 import { HostedZone, IHostedZone } from "aws-cdk-lib/aws-route53";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
 
 export class Route53Construct extends Construct {
-  public readonly hostedZone: IHostedZone;
-
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    this.hostedZone = this.createHostedZone();
+    this.createHostedZone();
   }
 
   private createHostedZone(): IHostedZone {
-    return new HostedZone(this, "HostedZone", {
+    const hostedZone = new HostedZone(this, "HostedZone", {
       zoneName: "igorcruz.space",
     });
+
+    new StringParameter(this, "HostedZoneIdParameter", {
+      parameterName: "/route53/hosted-zone-arn",
+      stringValue: hostedZone.hostedZoneArn,
+    });
+
+    return hostedZone;
   }
 }
