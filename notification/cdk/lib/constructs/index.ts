5bb9662db3340437af6c8cb70a27f9b6e1fbadf1
igorcruzz <igorcruz.dev@gmail.com>
2025-06-06 13:02:05 -0300

    feat: export ApiConstruct from constructs index

--- notification/cdk/lib/constructs/index.ts
+++ notification/cdk/lib/constructs/index.ts
@@ -1,3 +1,4 @@
 export { LambdaConstruct } from "./lambda.construct";
 export { SNSConstruct } from "./sns.construct";
 export { SQSConstruct } from "./sqs.construct";
+export { ApiConstruct } from "./api.construct";
