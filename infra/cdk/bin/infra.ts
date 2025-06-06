34414db190b97513aadcfcdae89c484c25396f85
igorcruzz <igorcruz.dev@gmail.com>
2025-06-05 22:03:53 -0300

    refactor: remove unused Route53 and ACM stack imports in infra.ts

--- infra/cdk/bin/infra.ts
+++ infra/cdk/bin/infra.ts
@@ -1,16 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { Route53Stack } from "../lib/stacks/route53.stack";
-import { AcmStack } from "../lib/stacks/acm-stack";
-import { ApiStack } from "../lib/stacks/api-stack";
+import { ApiStack } from "../lib/stacks/infra.stack";
 
 const app = new cdk.App();
 
-new Route53Stack(app, "Route53Stack");
-new AcmStack(app, "AcmStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
 new ApiStack(app, "ApiStack");
