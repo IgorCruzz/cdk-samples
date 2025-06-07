38534032817df652d039fdd3005fadbc6ebc9d66
igorcruzz <igorcruz.dev@gmail.com>
2025-06-07 17:31:02 -0300

    feat: implement putItem method in DynamoDB class for single item insertion

--- sheet-parse/lambda/shared/dynamodb.ts
+++ sheet-parse/lambda/shared/dynamodb.ts
@@ -2,17 +2,45 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   BatchWriteCommandInput,
   BatchWriteCommand,
+  PutCommandInput,
+  PutCommand,
 } from "@aws-sdk/lib-dynamodb";
 import { CustomerType } from "../schema/customer.schema";
 import KSUID from "ksuid";
 
 const client = new DynamoDBClient({});
 
+type FileProps = {
+  key: string;
+  size: number;
+};
+
 export interface DynamoDBInterface {
   putBatchItem: ({ data }: { data: CustomerType[] }) => Promise<void>;
+  putItem: (item: FileProps) => Promise<void>;
 }
 
 export class DynamoDB implements DynamoDBInterface {
+  async putItem(item: FileProps): Promise<void> {
+    const { key } = item;
+
+    const params: PutCommandInput = {
+      TableName: process.env.TABLE_NAME as string,
+      Item: {
+        ...item,
+        PK: `ARCHIVE#${key}`,
+        SK: `METADATA#${key}`,
+        ...item,
+        CreatedAt: new Date().toISOString(),
+      },
+      ConditionExpression: "attribute_not_exists(PK)",
+    };
+
+    const command = new PutCommand(params);
+
+    await client.send(command);
+  }
+
   async putBatchItem({ data }: { data: CustomerType[] }): Promise<void> {
     const customers = await Promise.all(
       data.map(async (item) => {
