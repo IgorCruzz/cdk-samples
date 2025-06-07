fcc4521efdca5fe593e3445bc67acb2c590d1f53
igorcruzz <igorcruz.dev@gmail.com>
2025-06-06 21:30:03 -0300

    feat: enhance DomainNameConstruct to accept certificate as a property

--- infra/cdk/lib/constructs/domain-name.construct.ts
+++ infra/cdk/lib/constructs/domain-name.construct.ts
@@ -1,8 +1,19 @@
 import { Construct } from "constructs";
-import { DomainName } from "aws-cdk-lib/aws-apigateway";
+import { DomainName, EndpointType } from "aws-cdk-lib/aws-apigateway";
+import { ICertificate } from "aws-cdk-lib/aws-certificatemanager";
+
+interface DomainNameProps {
+  certificate: ICertificate;
+}
 
 export class DomainNameConstruct extends Construct {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, readonly props: DomainNameProps) {
     super(scope, id);
+
+    new DomainName(this, "domain-name", {
+      domainName: "api.igorcruz.space",
+      certificate: this.props.certificate,
+      endpointType: EndpointType.REGIONAL,
+    });
   }
 }
