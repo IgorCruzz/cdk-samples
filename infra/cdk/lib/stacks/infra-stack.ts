8e6e2c9c5807c7eb0a18ef381e3fed425e5c00a2
igorcruzz <igorcruz.dev@gmail.com>
2025-06-06 22:05:48 -0300

    refactor: remove unused SQS and SNS resources from InfraStack

--- infra/cdk/lib/stacks/infra-stack.ts
+++ infra/cdk/lib/stacks/infra-stack.ts
@@ -1,19 +1,17 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import * as sns from "aws-cdk-lib/aws-sns";
-import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
-import * as sqs from "aws-cdk-lib/aws-sqs";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { CertificateConstruct, DomainNameConstruct } from "../constructs";
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, "InfraQueue", {
-      visibilityTimeout: Duration.seconds(300),
+    const { certificate } = new CertificateConstruct(
+      this,
+      "construct-certificate"
+    );
+    new DomainNameConstruct(this, "construct-domain-Name", {
+      certificate,
     });
-
-    const topic = new sns.Topic(this, "InfraTopic");
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
