a6f112d382f520ff6ce328838d97556cfb565596
igorcruzz <igorcruz.dev@gmail.com>
2025-06-07 23:38:31 -0300

    refactor: replace DynamoDB with repository methods in extractDataHandler

--- sheet-parse/lambda/extract-data/handler.ts
+++ sheet-parse/lambda/extract-data/handler.ts
@@ -1,15 +1,19 @@
 import { S3Event } from "aws-lambda";
 import { S3 } from "../shared/s3";
-import { DynamoDB } from "../shared/dynamodb";
+import { ArchiveRepository } from "../repository/archive.repository";
+import { CustomerRepository } from "../repository/customer.repository";
 import { ExtractDataService } from "./extract-data.service";
 
 export const extractDataHandler = async (event: S3Event) => {
-  console.log({ event });
-
   for (const record of event.Records) {
     const s3 = new S3();
-    const dynamoDB = new DynamoDB();
-    const extractDataService = new ExtractDataService(s3, dynamoDB);
+    const archiveRepository = new ArchiveRepository();
+    const customerRepository = new CustomerRepository();
+    const extractDataService = new ExtractDataService(
+      s3,
+      customerRepository,
+      archiveRepository
+    );
 
     return extractDataService.extract({ s3Record: record });
   }
