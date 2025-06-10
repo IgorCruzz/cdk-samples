8a0813e2d83d4be67970b5ef5497fba94028a27b
igorcruzz <igorcruz.dev@gmail.com>
2025-06-09 22:21:09 -0300

    refactor: remove KSUID dependency and update primary key generation in customer save method

--- sheet-parse/lambda/repository/customer.repository.ts
+++ sheet-parse/lambda/repository/customer.repository.ts
@@ -4,7 +4,6 @@ import {
   BatchWriteCommand,
 } from "@aws-sdk/lib-dynamodb";
 import { CustomerType } from "../schema/customer.schema";
-import KSUID from "ksuid";
 
 const client = new DynamoDBClient({});
 
@@ -16,13 +15,12 @@ export class CustomerRepository implements ICustomerRepository {
   async save({ data }: { data: CustomerType[] }): Promise<void> {
     const customers = await Promise.all(
       data.map(async (item) => {
-        const ID = (await KSUID.random()).string;
         return {
           PutRequest: {
             Item: {
+              PK: `CUSTOMER#${item.cnpj}`,
+              SK: `METADATA#${item.cnpj}`,
               ...item,
-              PK: `CUSTOMER#${ID}`,
-              SK: `METADATA#${ID}`,
               CreatedAt: new Date().toISOString(),
             },
           },
