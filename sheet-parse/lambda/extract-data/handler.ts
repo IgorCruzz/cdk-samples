a7b9bbbb949c25fc0b398106126058dc4acaad2f
igorcruzz <igorcruz.dev@gmail.com>
2025-06-14 18:04:20 -0300

    fix: add SendNotification dependency to ExtractDataService in extractDataHandler

--- sheet-parse/lambda/extract-data/handler.ts
+++ sheet-parse/lambda/extract-data/handler.ts
@@ -3,16 +3,19 @@ import { S3 } from "../shared/s3";
 import { ArchiveRepository } from "../repository/archive.repository";
 import { CustomerRepository } from "../repository/customer.repository";
 import { ExtractDataService } from "./extract-data.service";
+import { SendNotification } from "../shared/send-notification";
 
 export const extractDataHandler = async (event: S3Event) => {
   for (const record of event.Records) {
     const s3 = new S3();
     const archiveRepository = new ArchiveRepository();
     const customerRepository = new CustomerRepository();
+    const sendNotification = new SendNotification();
     const extractDataService = new ExtractDataService(
       s3,
       customerRepository,
-      archiveRepository
+      archiveRepository,
+      sendNotification
     );
 
     return extractDataService.extract({ s3Record: record });
