7d1a8f11962fd492ba90a15bc75ec7fb613458f9
igorcruzz <igorcruz.dev@gmail.com>
2025-06-14 18:04:37 -0300

    feat: implement SendNotification class for handling notification sending

---
+++ sheet-parse/lambda/shared/send-notification.ts
@@ -0,0 +1,38 @@
+export interface ISendNotification {
+  send(message: sendNotificationInput): Promise<sendNotificationOutput>;
+}
+
+export type sendNotificationInput = {
+  message: string;
+};
+
+export type sendNotificationOutput = Response;
+
+export class SendNotification implements ISendNotification {
+  send = async (
+    message: sendNotificationInput
+  ): Promise<sendNotificationOutput> => {
+    const response = await fetch(`${process.env.API_URL}/notifications`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        notifications: [
+          {
+            service: "EMAIL",
+            title: "File processed",
+            message,
+          },
+          {
+            service: "WHATSAPP",
+            title: "File processed",
+            message,
+          },
+        ],
+      }),
+    });
+
+    return response;
+  };
+}
