6a49c93664852372837a0a87f21400e564df825b
igorcruzz <igorcruz.dev@gmail.com>
2025-06-14 18:08:53 -0300

    fix: update basePath in BasePathMapping from 'sheet-parse' to 'archives'

--- sheet-parse/cdk/lib/constructs/api.construct.ts
+++ sheet-parse/cdk/lib/constructs/api.construct.ts
@@ -92,7 +92,7 @@ export class ApiConstruct extends Construct {
     new BasePathMapping(this, "sheet-parse-mapping", {
       domainName: domain,
       restApi: this.api,
-      basePath: "sheet-parse",
+      basePath: "archives",
     });
   }
 }
