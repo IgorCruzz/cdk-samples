b0b60c0d22324e5415e7a22f56461a975d95fb32
igorcruzz <igorcruz.dev@gmail.com>
2025-06-17 00:42:01 -0300

    fix: correct import path for FrontendStack in frontend.ts

--- frontend/cdk/bin/frontend.ts
+++ frontend/cdk/bin/frontend.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { FrontendStack } from '../lib/frontend-stack';
+import * as cdk from "aws-cdk-lib";
+import { FrontendStack } from "../lib/stacks/frontend-stack";
 
 const app = new cdk.App();
-new FrontendStack(app, 'FrontendStack');
+new FrontendStack(app, "FrontendStack");
