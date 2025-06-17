d815d795623d7713d05f950c087d0a885fde7ca6
igorcruzz <igorcruz.dev@gmail.com>
2025-06-16 23:59:33 -0300

    feat: instantiate S3Construct in FrontendStack for static website setup

--- frontend/cdk/lib/stacks/frontend-stack.ts
+++ frontend/cdk/lib/stacks/frontend-stack.ts
@@ -1,8 +1,11 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { S3Construct } from "../constructs/s3.construct";
 
 export class FrontendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    new S3Construct(this, "S3Construct");
   }
 }
