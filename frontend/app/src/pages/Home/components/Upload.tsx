4c21feef0666f319f554959acc185ad62d6d95c5
igorcruzz <igorcruz.dev@gmail.com>
2025-06-17 22:27:04 -0300

    feat: refactor Upload component layout for improved structure and styling

--- frontend/app/src/pages/Home/components/Upload.tsx
+++ frontend/app/src/pages/Home/components/Upload.tsx
@@ -1,15 +1,15 @@
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card" 
+import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card" 
 
 export function Upload() { 
   return ( 
-      <Card className="w-1/2">
-      <CardHeader>
-        <CardTitle>Upload a File</CardTitle>
+    <div className="w-1/2"> 
+      <Card>
+      <CardHeader className="item-center justify-center">
+        <CardTitle className="text-center">Upload a File</CardTitle>
         <CardDescription>Select a file to upload and click the submit button.</CardDescription>
       </CardHeader>
-      <CardContent> 
-      </CardContent>
       </Card>    
+    </div>
   )
 }
 
