33b8ed4bce1c41db32b4f8e928a1c6d28663bab5
igorcruzz <igorcruz.dev@gmail.com>
2025-06-17 22:21:09 -0300

    feat: implement Upload component with card layout and description

--- frontend/app/src/pages/Home/components/Upload.tsx
+++ frontend/app/src/pages/Home/components/Upload.tsx
@@ -1,6 +1,16 @@
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card" 
+
+export function Upload() { 
+  return ( 
+      <Card className="w-1/2">
+      <CardHeader>
+        <CardTitle>Upload a File</CardTitle>
+        <CardDescription>Select a file to upload and click the submit button.</CardDescription>
+      </CardHeader>
+      <CardContent> 
+      </CardContent>
+      </Card>    
+  )
+}
+
  
-export const Upload = () => {
-  return <>
-  Upload
-  </>
-} 
