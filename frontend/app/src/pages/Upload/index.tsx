d936f171f13e1b9e45d577978d11d63112770ce5
igorcruzz <igorcruz.dev@gmail.com>
2025-06-17 21:21:31 -0300

    feat: add Upload component with Historic and Progess imports

---
+++ frontend/app/src/pages/Upload/index.tsx
@@ -0,0 +1,14 @@
+import {
+  Historic,
+  Progess
+} from './components'
+
+export default function Upload() {
+  return (
+    <div>
+      UPLOAD
+      <Progess />
+      <Historic /> 
+    </div>
+  )
+}
