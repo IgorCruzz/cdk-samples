8672a298873abab1986502f197a88cf9aee0a2ca
igorcruzz <igorcruz.dev@gmail.com>
2025-06-17 22:20:33 -0300

    feat: wrap Home component with Layout in App component

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,12 +1,18 @@
  
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Home from './pages/Home';
+import Layout from './components/AppLayout';
+
 
 export default function App() {
   return ( 
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={
+            <Layout>
+              <Home />
+            </Layout>
+            } />
         </Routes>
       </BrowserRouter> 
   );
