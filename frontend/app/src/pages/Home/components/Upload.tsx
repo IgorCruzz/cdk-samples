0abda72997e0f51f341de0f941629f5d2ce0fc5c
igorcruzz <igorcruz.dev@gmail.com>
2025-06-17 22:35:18 -0300

    feat: implement Upload component with form handling and file input

--- frontend/app/src/pages/Home/components/Upload.tsx
+++ frontend/app/src/pages/Home/components/Upload.tsx
@@ -1,6 +1,19 @@
-import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card" 
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card" 
+import { Form } from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
 
 export function Upload() { 
+  const form = useForm<{ file: File | null }>({
+    defaultValues: {
+      file: null,
+    },
+  });
+
+  const onSubmit = ({ file }: { file: File | null }) => {
+    console.log(file);
+    // Handle file upload logic here
+  };
+
   return ( 
     <div className="w-1/2"> 
       <Card>
@@ -8,6 +21,17 @@ export function Upload() {
         <CardTitle className="text-center">Upload a File</CardTitle>
         <CardDescription>Select a file to upload and click the submit button.</CardDescription>
       </CardHeader>
+
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>  
+            <input type="file" 
+            {...form.register('file')}
+            />
+            <button type="submit">Submit</button>s
+          </form>          
+        </Form>
+      </CardContent>
       </Card>    
     </div>
   )
