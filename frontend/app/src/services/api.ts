0cdfa7cb415f6bb7eec6461d3f13bf71cf78a6d0
igorcruzz <igorcruz.dev@gmail.com>
2025-06-18 22:22:36 -0300

    fix: correct casing of 'X-Api-Key' in API headers

--- frontend/app/src/services/api.ts
+++ frontend/app/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'x-api-key': import.meta.env.VITE_API_KEY,
+    'X-Api-Key': import.meta.env.VITE_API_KEY,
   }
 })
 
