31b05ab0c1b6656cc80749c541222f4979a3308b
igorcruzz <igorcruz.dev@gmail.com>
2025-06-18 22:30:40 -0300

    feat: add CORS headers to presigned URL response

--- sheet-parse/lambda/generate-presigned-url/generate-presigned-url.service.ts
+++ sheet-parse/lambda/generate-presigned-url/generate-presigned-url.service.ts
@@ -18,6 +18,11 @@ export class GeneratePresignedUrlService
 
       return {
         statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Methods": "POST, OPTIONS",
+        },
         body: JSON.stringify({
           url: preSignedUrl.url,
           key: preSignedUrl.key,
