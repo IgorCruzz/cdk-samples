38eff57420209e16c6cc2c72184631f3bea9dd21
igorcruzz <igorcruz.dev@gmail.com>
2025-06-18 22:01:47 -0300

    feat: wrap Routes in QueryClientProvider for improved data fetching

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -2,18 +2,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Home from './pages/Home';
 import Layout from './components/AppLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 
 export default function App() {
   return ( 
       <BrowserRouter>
-        <Routes>
+      <QueryClientProvider client={queryClient}>
+         <Routes>
           <Route path="/" element={
             <Layout>
               <Home />
             </Layout>
             } />
         </Routes>
+      </QueryClientProvider>       
       </BrowserRouter> 
   );
 }
