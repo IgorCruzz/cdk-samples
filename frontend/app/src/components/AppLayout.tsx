f9c15d09df97fb0cd9a00f003a7131984b4958f4
igorcruzz <igorcruz.dev@gmail.com>
2025-06-20 11:31:02 -0300

    fix: update header title to 'Sheet Parse'

--- frontend/app/src/components/AppLayout.tsx
+++ frontend/app/src/components/AppLayout.tsx
@@ -2,7 +2,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen w-screen bg-[#181920] flex flex-col justify-between">
       <header className="flex items-center justify-center text-zinc-400">
-        <h1>Upload</h1>
+        <h1>Sheet Parse</h1>
       </header>
 
       <main className="w-screen h-screen p-4">{children}</main>
