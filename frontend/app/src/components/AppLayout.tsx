2860edfa32ee174773f55d2b6b7bb8831f6d8024
igorcruzz <igorcruz.dev@gmail.com>
2025-06-21 12:57:00 -0300

    fix: refactor AppLayout component for consistent styling in header and footer

--- frontend/app/src/components/AppLayout.tsx
+++ frontend/app/src/components/AppLayout.tsx
@@ -1,15 +1,17 @@
-export default function AppLayout({ children }: { children: React.ReactNode }) {
+const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen w-screen bg-[#181920] flex flex-col justify-between">
-      <header className="flex items-center justify-center text-zinc-400">
+      <header className="flex items-center justify-center text-zinc-400 bg-[#121314]">
         <h1>Sheet Parse</h1>
       </header>
 
       <main className="w-screen h-screen p-4">{children}</main>
 
-      <footer className="flex items-center justify-center text-zinc-400">
+      <footer className="flex items-center justify-center text-zinc-400 bg-[#121314]">
         © 2025 - Todos os direitos reservados
       </footer>
     </div>
   );
 }
+
+export default AppLayout;
