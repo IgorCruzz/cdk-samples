3af622ff2de09be901f699d6b03410a341e2b904
igorcruzz <igorcruz.dev@gmail.com>
2025-06-21 16:02:13 -0300

    feat: replace header title with logo in AppLayout component

--- frontend/app/src/components/AppLayout.tsx
+++ frontend/app/src/components/AppLayout.tsx
@@ -1,8 +1,10 @@
+import Logo from '@/assets/sheetparse-logo.png';
+
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen w-screen bg-[#181920] flex flex-col justify-between">
       <header className="flex items-center justify-center text-zinc-400 bg-[#121314]">
-        <h1>Sheet Parse</h1>
+        <img src={Logo} alt="SheetParse Logo" className="h-20 w-20 bg-transparent" />
       </header>
 
       <main className="w-screen h-screen p-4">{children}</main>
