c34ec4679b630321a238f599a2589561432f3f82
igorcruzz <igorcruz.dev@gmail.com>
2025-06-21 20:49:53 -0300

    fix: correct formatting in Home component for consistent code style

--- frontend/app/src/pages/Home/index.tsx
+++ frontend/app/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { Upload, Historic } from './components';
 
-export default function Home() {
+export default function Home() { 
   return (
     <div className="flex w-full h-full gap-2 p-4">
       <Upload />
