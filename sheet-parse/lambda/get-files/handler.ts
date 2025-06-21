7ccbd78b4fef7be0d484e22a1a411e0c5d13d3be
igorcruzz <igorcruz.dev@gmail.com>
2025-06-21 13:47:15 -0300

    feat: rename extractDataHandler to getFilesDataHandler for clarity

--- sheet-parse/lambda/get-files/handler.ts
+++ sheet-parse/lambda/get-files/handler.ts
@@ -1,8 +1,7 @@
-import { S3Event } from "aws-lambda";
 import { ArchiveRepository } from "../repository/archive.repository";
 import { GetFilesServices } from "./get-files.services";
 
-export const extractDataHandler = async (event: S3Event) => {
+export const getFilesDataHanlder = async () => {
   const archiveRepository = new ArchiveRepository();
   const getFilesServices = new GetFilesServices(archiveRepository);
 
