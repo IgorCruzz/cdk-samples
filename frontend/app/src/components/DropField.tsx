24090ee254a4a3ce2c40ee44560b86e1319444a0
igorcruzz <igorcruz.dev@gmail.com>
2025-06-21 20:08:06 -0300

    fix: clean up formatting and correct file type acceptance in DropField component

--- frontend/app/src/components/DropField.tsx
+++ frontend/app/src/components/DropField.tsx
@@ -1,14 +1,12 @@
 import {
-  Controller, 
-  type UseControllerProps, 
+  Controller,
+  type UseControllerProps,
 } from "react-hook-form";
 import { useDropzone } from "react-dropzone";
- 
-
 
 const DropField = ({
-   ...controllerProps
-}: UseControllerProps) =>{
+  ...controllerProps
+}: UseControllerProps) => {
   return (
     <Controller
       {...controllerProps}
@@ -20,41 +18,48 @@ const DropField = ({
         const { getRootProps, getInputProps, isDragActive } = useDropzone({
           onDrop,
           multiple: false,
-          accept: { "application/pdf": [".csv"] }
+          accept: {
+            "text/csv": [".csv"],
+          },
         });
 
         const files = field.value ? [field.value as File] : [];
 
         return (
-          <div className="w-full"> 
+          <div className="w-full">
             <div
               {...getRootProps()}
-              className="w-full p-6 border-2 border-dashed rounded-md cursor-pointer text-center bg-gray-100 hover:brightness-75"
+              className="
+                w-full p-6 border-2 border-dashed rounded-md cursor-pointer text-center
+                bg-[--muted] text-[--muted-foreground] 
+                hover:brightness-75
+                dark:bg-[--muted-foreground] dark:text-[--muted]
+              "
             >
               <input {...getInputProps()} />
               {isDragActive ? (
                 <p>Drag a file here...</p>
               ) : files.length > 0 ? (
-                <ul className="text-sm text-gray-600">
+                <ul className="text-sm">
                   {files.map((file, i) => (
                     <li key={i}>{file.name}</li>
                   ))}
                 </ul>
               ) : (
-                <p>
-                  Drag and drop a file here, or click to select a file
-                </p>
+                <p>Drag and drop a file here, or click to select a file</p>
               )}
             </div>
 
             {fieldState.error && (
-              <p className="text-sm text-red-500">{fieldState.error.message}</p>
+              <p className="text-sm text-destructive mt-1">
+                {fieldState.error.message}
+              </p>
             )}
           </div>
         );
       }}
     />
   );
-}
+};
 
-export default DropField
+export default DropField;
