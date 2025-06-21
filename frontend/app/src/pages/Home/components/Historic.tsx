fa013f977705a3cc9e9fef77531fe4892c1f70a5
igorcruzz <igorcruz.dev@gmail.com>
2025-06-21 20:08:13 -0300

    fix: clean up imports and improve component structure in Historic component

--- frontend/app/src/pages/Home/components/Historic.tsx
+++ frontend/app/src/pages/Home/components/Historic.tsx
@@ -1,30 +1,29 @@
-import { Card, CardContent,   CardHeader, CardTitle } from '@/components/ui/card';
-import React from 'react';
-import { useQuery } from '@tanstack/react-query'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { useQuery } from '@tanstack/react-query';
 import { files } from '@/services/endpoints/files';
 
-export const Historic: React.FC = () => {
+export const Historic = () => {
   const { data } = useQuery({
     queryKey: [],
-    queryFn: files.getFiles, 
+    queryFn: files.getFiles,
     refetchOnWindowFocus: false,
     retry: false,
   });
 
   console.log({ data });
-  
 
   return (
-    <div className="w-1/2 text-amber-50 h-screen"> 
-      <Card className="bg-[#121314] h-screen" >
-      <CardHeader className="item-center justify-center">
-        <CardTitle className="text-center text-amber-50">Historico</CardTitle>
-       </CardHeader>
+    <div className="w-1/2 h-full">
+      <Card className="border border-[--border]">
+        <CardHeader className="items-center justify-center">
+          <CardTitle className="text-center">Histórico</CardTitle>
+        </CardHeader>
 
-      <CardContent className="flex flex-col items-center justify-center">
-       a
-      </CardContent>
-      </Card>    
+        <CardContent className="flex flex-col items-center justify-center">
+          {/* Conteúdo do histórico aqui */}
+          a
+        </CardContent>
+      </Card>
     </div>
-  )
-} 
+  );
+};
