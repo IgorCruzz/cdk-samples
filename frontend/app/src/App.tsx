f71f785984078492c83944124853d817b8ebbd0f
igorcruzz <igorcruz.dev@gmail.com>
2025-06-21 12:56:54 -0300

    feat: add Toaster component for notifications in App component

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -3,6 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Layout from './components/AppLayout';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'sonner';
+
+
+
+
+
+
+
+
+
+
 
 const queryClient = new QueryClient();
 
@@ -10,6 +21,7 @@ export default function App() {
   return ( 
       <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        <Toaster richColors position="top-right" />
          <Routes>
           <Route path="/" element={
             <Layout>
