32886821447f50d2133b5be49aabe4ee76f1f30a
igorcruzz <igorcruz.dev@gmail.com>
2025-06-21 20:07:59 -0300

    fix: update layout styles for AppLayout component to enhance visual consistency

--- frontend/app/src/components/AppLayout.tsx
+++ frontend/app/src/components/AppLayout.tsx
@@ -2,18 +2,18 @@ import Logo from '@/assets/sheetparse-logo.png';
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen w-screen bg-[#181920] flex flex-col justify-between">
-      <header className="flex items-center justify-center text-zinc-400 bg-[#121314]">
+    <div className="w-screen h-screen flex flex-col justify-between">
+      <header className="w-full flex justify-center items-center bg-[--primary]">
         <img src={Logo} alt="SheetParse Logo" className="h-20 w-20 bg-transparent" />
       </header>
 
-      <main className="w-screen h-screen p-4">{children}</main>
+      <main className="h-full">{children}</main>
 
-      <footer className="flex items-center justify-center text-zinc-400 bg-[#121314]">
+      <footer className="text-center">
         © 2025 - Todos os direitos reservados
       </footer>
     </div>
   );
-}
+};
 
 export default AppLayout;
