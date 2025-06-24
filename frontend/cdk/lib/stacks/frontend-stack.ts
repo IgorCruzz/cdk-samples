a61efa18710a3238554710c630626d8340438a04
igorcruzz <igorcruz.dev@gmail.com>
2025-06-24 19:16:46 -0300

    feat: instantiate S3Construct and pass bucket to CloudFrontConstruct in FrontendStack

--- frontend/cdk/lib/stacks/frontend-stack.ts
+++ frontend/cdk/lib/stacks/frontend-stack.ts
@@ -1,11 +1,15 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { S3Construct } from "../constructs/s3.construct";
+import { CloudFrontConstruct } from '../constructs/cloudfront.construct';
 
 export class FrontendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new S3Construct(this, "S3Construct");
+    const s3Construct = new S3Construct(this, "S3Construct");
+    new CloudFrontConstruct(this, "construct-cloudfront", {
+      bucket: s3Construct.bucket,
+    })
   }
 }
