c9a8357bde94f4f8b7da8260fd029ced19473a4f
igorcruzz <igorcruz.dev@gmail.com>
2025-06-24 19:17:13 -0300

    refactor: update generate method signature and include X-Api-Key in allowed headers

--- sheet-parse/lambda/generate-presigned-url/generate-presigned-url.service.ts
+++ sheet-parse/lambda/generate-presigned-url/generate-presigned-url.service.ts
@@ -1,6 +1,8 @@
 import { APIGatewayProxyResult } from "aws-lambda";
 import { IS3 } from "../shared/s3";
 
+type GeneratePresignedUrlServiceOutput = Promise<APIGatewayProxyResult>;
+
 export interface IGeneratePresignedUrlService {
   generate: () => Promise<APIGatewayProxyResult>;
 }
@@ -10,7 +12,7 @@ export class GeneratePresignedUrlService
 {
   constructor(private readonly s3: IS3) {}
 
-  async generate(): Promise<APIGatewayProxyResult> {
+  async generate(): GeneratePresignedUrlServiceOutput {
     try {
       const preSignedUrl = await this.s3.createPresignedUrl({
         bucket: process.env.BUCKET_NAME as string,
@@ -20,7 +22,8 @@ export class GeneratePresignedUrlService
         statusCode: 200,
         headers: {
           "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Headers":
+            "Content-Type, Authorization, X-Api-Key",
           "Access-Control-Allow-Methods": "POST, OPTIONS",
         },
         body: JSON.stringify({
