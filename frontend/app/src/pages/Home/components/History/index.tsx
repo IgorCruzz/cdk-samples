add57854c0fe156a0219cb6d5235902a807b9220
igorcruzz <igorcruz.dev@gmail.com>
2025-06-23 23:17:56 -0300

    feat: add History component with data fetching and loading state

---
+++ frontend/app/src/pages/Home/components/History/index.tsx
@@ -0,0 +1,40 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { useQuery } from '@tanstack/react-query';
+import { files } from '@/services/endpoints/files';
+import { DataTable } from './data-table';
+import { columns } from './columns';
+
+export const History = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ['files'],
+    queryFn: files.getFiles,
+    refetchOnWindowFocus: false,
+    retry: false,
+  }); 
+
+  return (
+    <div className="w-1/2 h-full">
+      <Card className="border border-[--border]  h-full">
+        <CardHeader className="items-center justify-center">
+          <CardTitle className="text-center">History</CardTitle>
+        </CardHeader>
+
+        <CardContent className="w-full flex flex-col items-center justify-center">
+          {isLoading ? (
+            <div className="flex items-center justify-center h-full">
+              <span>Loading...</span>
+            </div>
+          ) : (
+             <DataTable
+            columns={columns}
+            data={data?.data}
+          />
+          )}
+         
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+ 
