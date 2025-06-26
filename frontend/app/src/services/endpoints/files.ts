642d741b231345298cff492130ec4394fcbaee51
igorcruzz <igorcruz.dev@gmail.com>
2025-06-25 21:43:25 -0300

    feat: update getFiles method to accept startKey parameter for improved data retrieval

--- frontend/app/src/services/endpoints/files.ts
+++ frontend/app/src/services/endpoints/files.ts
@@ -4,7 +4,11 @@ export const files = {
   preSignedUrl: async () => {
     return await api.post('files');
   }, 
-  getFiles: async () => {
-    return await api.get('files');
+  getFiles: async ({ startKey }: { startKey?: string | null}) => {
+    return await api.get('files', {
+      params: {
+        startKey
+      }
+    });
   }
 }
