0f10102a9ee9041c3fb4e883e7a6f579b9b708f1
igorcruzz <igorcruz.dev@gmail.com>
2025-06-25 22:18:58 -0300

    fix: remove background color from Home component layout

--- frontend/app/src/pages/Home/index.tsx
+++ frontend/app/src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import { Upload, History, Graphic } from './components';
 export default function Home() { 
   return (
     <div className="flex w-full h-full gap-2 p-4">
-      <div className="w-1/2 h-full bg-amber-300 flex flex-col gap-2">
+      <div className="w-1/2 h-full flex flex-col gap-2">
       <Upload />
       <Graphic />
       </div>      
