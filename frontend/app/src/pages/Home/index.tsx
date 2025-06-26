f423f3cb2f006bc10c6c09d39ac989e94cf46064
igorcruzz <igorcruz.dev@gmail.com>
2025-06-25 22:18:02 -0300

    feat: add Graphic component to Home page layout

--- frontend/app/src/pages/Home/index.tsx
+++ frontend/app/src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
-import { Upload, History } from './components';
+import { Upload, History, Graphic } from './components';
 
 export default function Home() { 
   return (
     <div className="flex w-full h-full gap-2 p-4">
+      <div className="w-1/2 h-full bg-amber-300 flex flex-col gap-2">
       <Upload />
-      <History />
+      <Graphic />
+      </div>      
+
+      <History />     
     </div>
   );
 }
