70c4257ca1348fe0e465c5eb74c7c50da366918c
igorcruzz <igorcruz.dev@gmail.com>
2025-06-27 22:11:16 -0300

    refactor: update div class for full width in Home component

--- frontend/app/src/pages/Home/index.tsx
+++ frontend/app/src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import { Upload, History, Graphic } from './components';
 export default function Home() { 
   return (
     <div className="flex flex-col gap-2 justify-center lg:flex-row">
-      <div className="w-1/2 flex flex-col gap-2">
+      <div className="w-full lg:w-1/2 flex flex-col gap-2">
       <Upload />
       <Graphic />
       </div>
