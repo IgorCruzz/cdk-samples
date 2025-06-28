15c061d55cbe56a8bfcdd432e2274e8453f464e0
igorcruzz <igorcruz.dev@gmail.com>
2025-06-28 14:25:01 -0300

    refactor: update AppLayout header to use logo image and adjust footer height for consistency

--- frontend/app/src/components/AppLayout.tsx
+++ frontend/app/src/components/AppLayout.tsx
@@ -1,16 +1,17 @@
+import Logo from '@/assets/logo.png';
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
   <div className="min-h-screen flex flex-col justify-between">
-  <header className="h-[100px] flex items-center justify-center">
-    <p>SHEET PARSE</p>
+  <header className="h-[50px] flex items-center justify-center bg-[oklch(15.823%_0.01178_260.646)] p-5">
+    <img src={Logo} alt="Logo" height={80} width={80} />
   </header>
 
   <main className="flex-1 p-8">
     {children}
   </main>
 
-  <footer className="h-[100px] flex items-center justify-center">
+  <footer className="h-[50px] flex items-center justify-center bg-[oklch(15.823%_0.01178_260.646)]">
     Igor Cruz
   </footer>
 </div>
