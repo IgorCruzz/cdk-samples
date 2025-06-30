99d24467c77a2129225831ab7a0d9ec456499f3e
igorcruzz <igorcruz.dev@gmail.com>
2025-06-30 20:28:40 -0300

    refactor: adjust header height and logo size in AppLayout for consistency

--- frontend/app/src/components/AppLayout.tsx
+++ frontend/app/src/components/AppLayout.tsx
@@ -5,8 +5,8 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
   <div className="min-h-screen flex flex-col justify-between">
   <div className="absolute top-0 left-0 w-full h-[4px] rounded-tl-4xl bg-gradient-to-r from-green-500 to-white z-10" />
 
-  <header className="h-[150px] flex items-center justify-center bg-[oklch(15.823%_0.01178_260.646)] p-5">
-    <img src={Logo} alt="Logo" height={180} width={180} />
+  <header className="h-[100px] flex items-center justify-center bg-[oklch(15.823%_0.01178_260.646)] p-5">
+    <img src={Logo} alt="Logo" height={100} width={100} />
   </header>
 
   <main className="flex-1 p-8">
