a4c1deed4672d92fa1d6a2c96098b290cfce6a73
igorcruzz <igorcruz.dev@gmail.com>
2025-06-30 21:46:17 -0300

    fix: update chart data structure and improve legend display in Graphic component

--- frontend/app/src/pages/Home/components/Graphic/index.tsx
+++ frontend/app/src/pages/Home/components/Graphic/index.tsx
@@ -1,11 +1,11 @@
 "use client"
- 
-import { Pie, PieChart } from "recharts"
+
+import { Pie, PieChart, Legend } from "recharts"
 
 import {
   Card,
   CardContent,
-  CardDescription, 
+  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
@@ -17,14 +17,11 @@ import {
 } from "@/components/ui/chart"
 
 const chartData = [
-  { browser: "completed", visitors: 100, fill: "var(--color-completed)" },
-  { browser: "failed", visitors: 20, fill: "var(--color-failed)" },
+  { name: "completed", value: 100, fill: "var(--color-completed)" },
+  { name: "failed", value: 20, fill: "var(--color-failed)" },
 ]
 
 const chartConfig = {
-  visitors: {
-    label: "Visitors",
-  },
   completed: {
     label: "Completed",
     color: "var(--chart-1)",
@@ -32,7 +29,7 @@ const chartConfig = {
   failed: {
     label: "Failed",
     color: "var(--chart-2)",
-  }, 
+  },
 } satisfies ChartConfig
 
 export function Graphic() {
@@ -40,24 +37,32 @@ export function Graphic() {
     <Card className="h-1/2 rounded-tl-4xl rounded-br-4xl border-t-green-500 border-t-4">
       <CardHeader className="text-center">
         <CardTitle>Statistic</CardTitle>
-        <CardDescription>  Overview of your file upload results, showing how many were completed successfully and how many failed.
+        <CardDescription>
+          Overview of your file upload results, showing how many were completed successfully and how many failed.
         </CardDescription>
       </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
           config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]"
+          className="mx-auto aspect-square max-h-[200px]"
         >
           <PieChart>
             <ChartTooltip
               cursor={false}
               content={<ChartTooltipContent hideLabel />}
             />
+            <Legend
+              layout="horizontal"
+              verticalAlign="bottom"
+              align="center"
+            />
             <Pie
               data={chartData}
-              dataKey="visitors"
-              nameKey="browser"
+              dataKey="value"
+              nameKey="name"
               stroke="0"
+              label={({ name, value }) => `${name}: ${value}`}
+
             />
           </PieChart>
         </ChartContainer>
