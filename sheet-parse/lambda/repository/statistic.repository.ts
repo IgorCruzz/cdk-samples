df80bd5647f1785f633995cb710b870128d82373
igorcruzz <igorcruz.dev@gmail.com>
2025-06-30 22:01:17 -0300

    refactor: reorganize types and improve method structure in StatisticRepository

--- sheet-parse/lambda/repository/statistic.repository.ts
+++ sheet-parse/lambda/repository/statistic.repository.ts
@@ -1,20 +1,52 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { UpdateCommandInput, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import {
+  UpdateCommandInput,
+  UpdateCommand,
+  GetCommandInput,
+  GetCommand,
+  GetCommandOutput,
+} from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
 
-type StatisticRepositoryInput = {
+type SaveInput = {
   type: "COMPLETED" | "FAILED";
 };
 
-type StatisticRepositoryOutput = Promise<void>;
+type SaveOutput = Promise<void>;
+
+type GetOutput = Promise<{
+  Completed: number;
+  Failed: number;
+}>;
 
 export interface IStatisticRepository {
-  save: (input: StatisticRepositoryInput) => StatisticRepositoryOutput;
+  save: (input: SaveInput) => SaveOutput;
+  get: () => GetOutput;
 }
 
 export class StatisticRepository implements IStatisticRepository {
-  async save(input: StatisticRepositoryInput): StatisticRepositoryOutput {
+  async get(): GetOutput {
+    const params: GetCommandInput = {
+      TableName: process.env.TABLE_NAME as string,
+      Key: {
+        PK: "STATISTIC",
+        SK: "STATISTIC",
+      },
+      ProjectionExpression: "Completed, Failed",
+    };
+
+    const command = new GetCommand(params);
+
+    const res: GetCommandOutput = await client.send(command);
+
+    return res.Item as {
+      Completed: number;
+      Failed: number;
+    };
+  }
+
+  async save(input: SaveInput): SaveOutput {
     const params: UpdateCommandInput = {
       TableName: process.env.TABLE_NAME as string,
       Key: {
