f38a35e4d9660506e001a479c3de82ac597d80bc
igorcruzz <igorcruz.dev@gmail.com>
2025-06-30 21:02:28 -0300

    refactor: inject StatisticRepository into StreamService for improved dependency management

--- sheet-parse/lambda/stream-statistic/handler.ts
+++ sheet-parse/lambda/stream-statistic/handler.ts
@@ -1,7 +1,9 @@
 import { DynamoDBStreamEvent } from "aws-lambda";
 import { StreamService } from "./stream-statistic.service";
+import { StatisticRepository } from "../repository/statistic.repository";
 
 export const handler = async (event: DynamoDBStreamEvent): Promise<void> => {
-  const streamService = new StreamService();
+  const statisticRepository = new StatisticRepository();
+  const streamService = new StreamService(statisticRepository);
   await streamService.stream(event);
 };
