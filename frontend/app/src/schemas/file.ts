ef1d2fbc6765c18d82b978086f841a38eba697fe
igorcruzz <igorcruz.dev@gmail.com>
2025-07-02 21:00:37 -0300

    refactor: update file validation logic in fileSchema for improved clarity and error messages

--- frontend/app/src/schemas/file.ts
+++ frontend/app/src/schemas/file.ts
@@ -1,11 +1,14 @@
 import { z } from 'zod'
 
 export const fileSchema = z.object({
-  file: z
-    .any()
-    .refine((file) => file instanceof File, { message: "Arquivo obrigatório." })
-    .refine((file) => file?.size > 0, { message: "Arquivo vazio." })
-    .refine((file) => ['text/csv'].includes(file?.type), { message: "Você deve enviar um arquivo CSV válido." })
-});
+  file: z.custom<File>((file) => {
+    if (!(file instanceof File)) return false;
+    if (file.size === 0) return false;
+    if (!['text/csv'].includes(file.type)) return false;
+    return true;
+  }, {
+    message: "You must upload a valid CSV file.",
+  })
+})
 
 export type fileType = z.infer<typeof fileSchema>
