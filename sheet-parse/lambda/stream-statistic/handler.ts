6ce6f83b31acf7ec59b22c8837b54f7c777bb29d
igorcruzz <igorcruz.dev@gmail.com>
2025-07-04 20:58:35 -0300

    fix: remove unused DynamoDB stream handler and service for cleaner codebase

--- sheet-parse/lambda/stream-statistic/handler.ts
+++
@@ -1,9 +0,0 @@
-import { DynamoDBStreamEvent } from "aws-lambda";
-import { StreamService } from "./stream-statistic.service";
-import { StatisticRepository } from "../repository/statistic.repository";
-
-export const handler = async (event: DynamoDBStreamEvent): Promise<void> => {
-  const statisticRepository = new StatisticRepository();
-  const streamService = new StreamService(statisticRepository);
-  await streamService.stream(event);
-};
