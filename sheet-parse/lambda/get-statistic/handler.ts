a8e01664627f7a97845383be2fcd59bd0402e65b
igorcruzz <igorcruz.dev@gmail.com>
2025-07-04 20:59:06 -0300

    fix: remove unused GetStatistic service and handler for cleaner codebase

--- sheet-parse/lambda/get-statistic/handler.ts
+++
@@ -1,9 +0,0 @@
-import { StatisticRepository } from "../repository/statistic.repository";
-import { GetStatisticServices } from "./get-statistic.service";
-
-export const handler = async () => {
-  const statisticRepository = new StatisticRepository();
-  const getFilesServices = new GetStatisticServices(statisticRepository);
-
-  return getFilesServices.getFiles();
-};
