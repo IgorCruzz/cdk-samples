a38ec4b05184ec0e0cf6ad2de47b4c244e048284
igorcruzz <igorcruz.dev@gmail.com>
2025-07-05 14:38:39 -0300

    refactor: streamline MongoDB connection logic and remove unused dbHelper

--- sheet-parse/lambda/extract-data/handler.ts
+++ sheet-parse/lambda/extract-data/handler.ts
@@ -4,23 +4,19 @@ import { ArchiveRepository } from "../shared/repository/archive.repository";
 import { CustomerRepository } from "../shared/repository/customer.repository";
 import { ExtractDataService } from "./extract-data.service";
 import { SendNotification } from "../shared/infra/send-notification";
-import { dbHelper } from "../shared/repository/db-helper";
 
-let isDbConnected = false;
+import { MongoClient } from "mongodb";
 
 const encodedPassword = encodeURIComponent(
   process.env.DOCDB_PASSWORD as string
 );
 
-const MONGO_URI = `mongodb://${process.env.DOCDB_USER}:${encodedPassword}@${process.env.DOCDB_URI}:27017/sheet-parse`;
+const MONGO_URI = `mongodb://${process.env.DOCDB_USER}:${encodedPassword}@${process.env.DOCDB_URI}:27017/`;
+
+const client = new MongoClient(MONGO_URI);
 
 export const extractDataHandler = async (event: S3Event) => {
-  console.log({ MONGO_URI });
-
-  if (!isDbConnected) {
-    await dbHelper.connect(MONGO_URI || "");
-    isDbConnected = true;
-  }
+  await client.db("admin").command({ listDatabases: 1 });
 
   for (const record of event.Records) {
     const s3 = new S3();
