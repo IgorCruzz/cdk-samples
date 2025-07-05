3cbb3036401c1c3234ad10331b1f28937d1895d1
igorcruzz <igorcruz.dev@gmail.com>
2025-07-04 21:14:15 -0300

    feat: add dbHelper for MongoDB connection and data mapping

---
+++ sheet-parse/lambda/repository/db-helper.ts
@@ -0,0 +1,29 @@
+import { MongoClient, Collection } from "mongodb";
+
+export const dbHelper = {
+  client: null as MongoClient | null,
+  uri: null as string | null,
+
+  async connect(uri: string): Promise<void> {
+    this.uri = uri;
+    this.client = await MongoClient.connect(uri);
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client?.close();
+    this.client = null;
+  },
+
+  getCollection(name: string): Collection | null {
+    return this.client?.db().collection(name) || null;
+  },
+
+  map: (data: any): any => {
+    const { _id, ...rest } = data;
+    return { ...rest, id: _id.toHexString() };
+  },
+
+  mapCollection: (collection: any[]): any[] => {
+    return collection.map((c) => dbHelper.map(c));
+  },
+};
