8540f2c9f8f3d863e9074e2b9484318e36e9432e
igorcruzz <igorcruz.dev@gmail.com>
2025-07-05 12:11:57 -0300

    refactor: add database connection logic to extractDataHandler

--- sheet-parse/lambda/extract-data/handler.ts
+++ sheet-parse/lambda/extract-data/handler.ts
@@ -4,8 +4,16 @@ import { ArchiveRepository } from "../shared/repository/archive.repository";
 import { CustomerRepository } from "../shared/repository/customer.repository";
 import { ExtractDataService } from "./extract-data.service";
 import { SendNotification } from "../shared/infra/send-notification";
+import { dbHelper } from "../shared/repository/db-helper";
+
+let isDbConnected = false;
 
 export const extractDataHandler = async (event: S3Event) => {
+  if (!isDbConnected) {
+    await dbHelper.connect(process.env.MONGODB_URI || "");
+    isDbConnected = true;
+  }
+
   for (const record of event.Records) {
     const s3 = new S3();
     const archiveRepository = new ArchiveRepository();
