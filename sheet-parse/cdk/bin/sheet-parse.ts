e5c7d15ee62cd6bfe3948ad32daadfee18aaa248
igorcruzz <igorcruz.dev@gmail.com>
2025-07-05 12:05:39 -0300

    feat: add environment configuration to SheetParseStack instantiation

--- sheet-parse/cdk/bin/sheet-parse.ts
+++ sheet-parse/cdk/bin/sheet-parse.ts
@@ -3,4 +3,9 @@ import * as cdk from "aws-cdk-lib";
 import { SheetParseStack } from "../lib/stacks/sheet-parse.stack";
 
 const app = new cdk.App();
-new SheetParseStack(app, "stack-sheet-parse");
+new SheetParseStack(app, "stack-sheet-parse", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
