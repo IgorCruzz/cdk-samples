2ec7d9a69acd80ce7b19e1700694dd409cec0b88
igorcruzz <igorcruz.dev@gmail.com>
2025-07-06 17:20:13 -0300

    refactor: implement MongoDB URI retrieval from AWS Secrets Manager in getFilesDataHandler

--- sheet-parse/lambda/get-files/handler.ts
+++ sheet-parse/lambda/get-files/handler.ts
@@ -2,7 +2,35 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 import { ArchiveRepository } from "../shared/repository/archive.repository";
 import { GetFilesServices } from "./get-files.services";
 
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+import { dbHelper } from "../shared/repository/db-helper";
+
+const secretsManager = new SecretsManagerClient();
+
+async function getMongoUri(): Promise<string> {
+  const res = await secretsManager.send(
+    new GetSecretValueCommand({
+      SecretId: "mongodb/uri",
+    })
+  );
+
+  const secret = JSON.parse(res.SecretString ?? "{}");
+  return secret["MONGO_URI"];
+}
+
+let isConnected = false;
+
 export const getFilesDataHanlder = async (event: APIGatewayProxyEvent) => {
+  if (!isConnected) {
+    const uri = await getMongoUri();
+    await dbHelper.connect(uri);
+    isConnected = true;
+  }
+
   const archiveRepository = new ArchiveRepository();
   const getFilesServices = new GetFilesServices(archiveRepository);
 
