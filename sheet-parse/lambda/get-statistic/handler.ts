236a27f3c079f5d5707c8a1d821ea71d2c2f807f
igorcruzz <igorcruz.dev@gmail.com>
2025-07-07 23:01:11 -0300

    feat: implement MongoDB connection handling in the handler

--- sheet-parse/lambda/get-statistic/handler.ts
+++ sheet-parse/lambda/get-statistic/handler.ts
@@ -1,7 +1,34 @@
 import { ArchiveRepository } from "../shared/repository/archive.repository";
 import { GetStatisticServices } from "./get-statistic.service";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+import { dbHelper } from "../shared/repository/db-helper";
+
+const secretsManager = new SecretsManagerClient();
+
+async function getMongoUri(): Promise<string> {
+  const res = await secretsManager.send(
+    new GetSecretValueCommand({
+      SecretId: "mongodb/uri",
+    })
+  );
+
+  const secret = JSON.parse(res.SecretString ?? "{}");
+  return secret["MONGO_URI"];
+}
+
+let isConnected = false;
 
 export const handler = async () => {
+  if (!isConnected) {
+    const uri = await getMongoUri();
+    await dbHelper.connect(uri);
+    isConnected = true;
+  }
+
   const archiveRepository = new ArchiveRepository();
   const getFilesServices = new GetStatisticServices(archiveRepository);
 
