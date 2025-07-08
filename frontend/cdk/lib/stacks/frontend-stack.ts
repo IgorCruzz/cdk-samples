27c42b5c7356902ff1b8230b2b53a5ff9ec12cc9
igorcruzz <igorcruz.dev@gmail.com>
2025-07-08 15:11:01 -0300

    fix: correct import formatting and ensure proper closure in FrontendStack

--- frontend/cdk/lib/stacks/frontend-stack.ts
+++ frontend/cdk/lib/stacks/frontend-stack.ts
@@ -1,7 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { S3Construct } from "../constructs/s3.construct";
-import { CloudFrontConstruct } from '../constructs/cloudfront.construct';
+import { CloudFrontConstruct } from "../constructs/cloudfront.construct";
 
 export class FrontendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -10,6 +10,6 @@ export class FrontendStack extends Stack {
     const s3Construct = new S3Construct(this, "S3Construct");
     new CloudFrontConstruct(this, "construct-cloudfront", {
       bucket: s3Construct.bucket,
-    })
+    });
   }
 }
