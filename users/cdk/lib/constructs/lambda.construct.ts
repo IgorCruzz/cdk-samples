d92b608a061b3367643b9b2d341692fc911eaad5
igorcruzz <igorcruz.dev@gmail.com>
2025-07-08 23:25:59 -0300

    feat: implement LambdaConstruct with CRUD functions for user management

---
+++ users/cdk/lib/constructs/lambda.construct.ts
@@ -0,0 +1,161 @@
+import { Duration, Stack } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import {
+  Architecture,
+  LoggingFormat,
+  Runtime,
+  Tracing,
+} from "aws-cdk-lib/aws-lambda";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import { join } from "node:path";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+
+export class LambdaConstruct extends Construct {
+  public readonly getUsersFunction: NodejsFunction;
+  public readonly createUserFunction: NodejsFunction;
+  public readonly updateUserFunction: NodejsFunction;
+  public readonly deleteUserFunction: NodejsFunction;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.getUsersFunction = this.createGetUsersFunction();
+    this.createUserFunction = this.createCreateUserFunction();
+    this.updateUserFunction = this.createUpdateUserFunction();
+    this.deleteUserFunction = this.createDeleteUserFunction();
+  }
+
+  private createDeleteUserFunction() {
+    const fn = new NodejsFunction(this, "function-delete-user", {
+      memorySize: 128,
+      architecture: Architecture.X86_64,
+      runtime: Runtime.NODEJS_20_X,
+      timeout: Duration.seconds(30),
+      description: "A Lambda function to delete a user",
+      entry: join(__dirname, "../../../lambda/delete-user/handler.ts"),
+      handler: "deleteUserHandler",
+      bundling: {
+        minify: true,
+        sourceMap: true,
+        target: "es2020",
+      },
+      loggingFormat: LoggingFormat.JSON,
+      tracing: Tracing.ACTIVE,
+      logRetention: RetentionDays.ONE_WEEK,
+    });
+
+    const region = Stack.of(this).region;
+    const account = Stack.of(this).account;
+
+    fn.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["secretsmanager:GetSecretValue"],
+        resources: [
+          `arn:aws:secretsmanager:${region}:${account}:secret:mongodb/uri-*`,
+        ],
+      })
+    );
+    return fn;
+  }
+
+  private createUpdateUserFunction() {
+    const fn = new NodejsFunction(this, "function-update-user", {
+      memorySize: 128,
+      architecture: Architecture.X86_64,
+      runtime: Runtime.NODEJS_20_X,
+      timeout: Duration.seconds(30),
+      description: "A Lambda function to update a user",
+      entry: join(__dirname, "../../../lambda/update-user/handler.ts"),
+      handler: "updateUserHandler",
+      bundling: {
+        minify: true,
+        sourceMap: true,
+        target: "es2020",
+      },
+      loggingFormat: LoggingFormat.JSON,
+      tracing: Tracing.ACTIVE,
+      logRetention: RetentionDays.ONE_WEEK,
+    });
+    const region = Stack.of(this).region;
+    const account = Stack.of(this).account;
+
+    fn.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["secretsmanager:GetSecretValue"],
+        resources: [
+          `arn:aws:secretsmanager:${region}:${account}:secret:mongodb/uri-*`,
+        ],
+      })
+    );
+
+    return fn;
+  }
+
+  private createCreateUserFunction() {
+    const fn = new NodejsFunction(this, "function-create-user", {
+      memorySize: 128,
+      architecture: Architecture.X86_64,
+      runtime: Runtime.NODEJS_20_X,
+      timeout: Duration.seconds(30),
+      description: "A Lambda function to create a user",
+      entry: join(__dirname, "../../../lambda/create-user/handler.ts"),
+      handler: "createUserHandler",
+      bundling: {
+        minify: true,
+        sourceMap: true,
+        target: "es2020",
+      },
+      loggingFormat: LoggingFormat.JSON,
+      tracing: Tracing.ACTIVE,
+      logRetention: RetentionDays.ONE_WEEK,
+    });
+    const region = Stack.of(this).region;
+    const account = Stack.of(this).account;
+
+    fn.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["secretsmanager:GetSecretValue"],
+        resources: [
+          `arn:aws:secretsmanager:${region}:${account}:secret:mongodb/uri-*`,
+        ],
+      })
+    );
+
+    return fn;
+  }
+
+  private createGetUsersFunction() {
+    const fn = new NodejsFunction(this, "function-get-users", {
+      memorySize: 128,
+      architecture: Architecture.X86_64,
+      runtime: Runtime.NODEJS_20_X,
+      timeout: Duration.seconds(30),
+      description: "A Lambda function to get users data",
+      entry: join(__dirname, "../../../lambda/get-users/handler.ts"),
+      handler: "getUsersDataHandler",
+      bundling: {
+        minify: true,
+        sourceMap: true,
+        target: "es2020",
+      },
+      loggingFormat: LoggingFormat.JSON,
+      tracing: Tracing.ACTIVE,
+      logRetention: RetentionDays.ONE_WEEK,
+    });
+
+    const region = Stack.of(this).region;
+    const account = Stack.of(this).account;
+
+    fn.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["secretsmanager:GetSecretValue"],
+        resources: [
+          `arn:aws:secretsmanager:${region}:${account}:secret:mongodb/uri-*`,
+        ],
+      })
+    );
+
+    return fn;
+  }
+}
