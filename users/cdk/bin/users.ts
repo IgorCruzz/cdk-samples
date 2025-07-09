4a888a426e55027eebfe012956591952b4ed1f40
igorcruzz <igorcruz.dev@gmail.com>
2025-07-08 23:16:12 -0300

    fix: update UsersStack identifier to use lowercase for consistency

--- users/cdk/bin/users.ts
+++ users/cdk/bin/users.ts
@@ -3,4 +3,4 @@ import * as cdk from 'aws-cdk-lib';
 import { UsersStack } from '../lib/users-stack';
 
 const app = new cdk.App();
-new UsersStack(app, 'UsersStack');
+new UsersStack(app, 'stack-users');
