6d60d0aed4817055142e41e32e5f848594cf5dba
igorcruzz <igorcruz.dev@gmail.com>
2025-07-09 22:31:31 -0300

    feat: add SES, SNS, and Twilio integrations for notification services; enhance error handling and response structure in handler

--- notification/lambda/send-notification/handler.ts
+++ notification/lambda/send-notification/handler.ts
@@ -3,7 +3,36 @@ import { service } from "./notifier-send.service";
 import { NotifyType } from "../shared/types/notifier.type";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
-  const body: { notifications: NotifyType[] } = JSON.parse(event.body || "");
+  try {
+    const body: { notifications: NotifyType[] } = JSON.parse(event.body || "");
 
-  return await service({ notifications: body.notifications });
+    await service({ notifications: body.notifications });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Notifications sent successfully",
+      }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, X-Api-Key",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+      },
+    };
+  } catch (error) {
+    console.error("Error generating presigned URL:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Internal Server Error",
+      }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, X-Api-Key",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+      },
+    };
+  }
 };
--- notification/lambda/send-notification/notifier-send.service.ts
+++ notification/lambda/send-notification/notifier-send.service.ts
@@ -7,42 +7,25 @@ export const service = async ({
 }: {
   notifications: NotifyType[];
 }): Promise<APIGatewayProxyResult> => {
-  try {
-    await sns.publishBatchMessage({
-      data: notifications,
-      attributes: [
-        {
-          dataType: "String",
-          stringValue: "service",
-        },
-      ],
-    });
+  await sns.publishBatchMessage({
+    data: notifications,
+    attributes: [
+      {
+        dataType: "String",
+        stringValue: "service",
+      },
+    ],
+  });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Notifications sent successfully",
-      }),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers":
-          "Content-Type, Authorization, X-Api-Key",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
-      },
-    };
-  } catch (err) {
-    console.log(err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: "Internal Server Error",
-      }),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers":
-          "Content-Type, Authorization, X-Api-Key",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
-      },
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Notifications sent successfully",
+    }),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Api-Key",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+    },
+  };
 };
