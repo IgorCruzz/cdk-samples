cbeb7b3b561f2d5c40c44730564ebc5861a5211e
igorcruzz <igorcruz.dev@gmail.com>
2025-07-12 15:14:59 -0300

    fix: update header styling and logo dimensions for improved layout consistency

--- frontend/app/src/components/AppLayout.tsx
+++ frontend/app/src/components/AppLayout.tsx
@@ -5,8 +5,9 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
   <div className="min-h-screen flex flex-col justify-between">
   <div className="absolute top-0 left-0 w-full h-[4px] rounded-tl-4xl bg-gradient-to-r from-green-500 to-white z-10" />
 
-  <header className="h-[100px] flex items-center justify-center bg-[oklch(15.823%_0.01178_260.646)] p-5">
-    <img src={Logo} alt="Logo" height={100} width={100} />
+  <header className="h-[80px] flex items-center justify-between px-6 bg-gradient-to-r from-green-500 to-blue-500">
+        <img src={Logo} alt="Logo" height={60} width={60} />
+        <span className="text-white font-bold">Bem-vindo</span>
   </header>
 
   <main className="flex-1 p-8">
