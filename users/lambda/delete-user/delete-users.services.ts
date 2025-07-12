b66c86d4fa2209237bc47f66863a901142f6c3f8
igorcruzz <igorcruz.dev@gmail.com>
2025-07-12 00:36:39 -0300

    fix: update delete user service to include success status and data in responses

--- users/lambda/delete-user/delete-users.services.ts
+++ users/lambda/delete-user/delete-users.services.ts
@@ -1,21 +1,16 @@
 import { userRepository } from "../shared/repository/user.repository";
+import { Output } from "../shared/service/output";
 
-export const service = async ({
-  id,
-}: {
-  id: string;
-}): Promise<{
-  message: string;
-}> => {
+export const service = async ({ id }: { id: string }): Output => {
   const verifyUser = await userRepository.findById(id);
 
   if (!verifyUser) {
-    return { message: "User not found" };
+    return { message: "User not found", success: false, data: null };
   }
 
   await userRepository.delete({
     id,
   });
 
-  return { message: "User deleted successfully" };
+  return { message: "User deleted successfully", success: true, data: null };
 };
