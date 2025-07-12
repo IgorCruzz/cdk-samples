9e98eff2b3d304f0e3fe47965a44cbd9ea767551
igorcruzz <igorcruz.dev@gmail.com>
2025-07-12 15:24:49 -0300

    refactor: remove unused axios import from LoginPage component

--- frontend/app/src/pages/Auth/index.tsx
+++ frontend/app/src/pages/Auth/index.tsx
@@ -1,5 +1,5 @@
  import { useMutation } from "@tanstack/react-query";
-import axios, { AxiosError } from "axios";
+import { AxiosError } from "axios";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from '@/components/ui/input'; 
