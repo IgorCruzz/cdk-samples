c16de8e2cc8c721bf8c79b7a0828fd039a9fbabf
igorcruzz <igorcruz.dev@gmail.com>
2025-07-12 00:36:23 -0300

    fix: enhance create user service to include success status and data in responses

--- users/lambda/create-user/create-users.services.ts
+++ users/lambda/create-user/create-users.services.ts
@@ -1,16 +1,15 @@
 import { userRepository, Users } from "../shared/repository/user.repository";
+import { Output } from "../shared/service/output";
 
 type CreateUserInput = Users;
 
-export const service = async (
-  data: CreateUserInput
-): Promise<{ message: string }> => {
+export const service = async (data: CreateUserInput): Output => {
   const existingUser = await userRepository.findByEmail(data.email);
   if (existingUser) {
-    return { message: "Email already exists" };
+    return { message: "Email already exists", success: false, data: null };
   }
 
   await userRepository.save(data);
 
-  return { message: "User created successfully" };
+  return { message: "User created successfully", success: true, data: null };
 };
