127fb61b8d83dbd7abfebcfda768b671dabc4ec9
igorcruzz <igorcruz.dev@gmail.com>
2025-07-13 19:38:05 -0300

    feat: implement signin handler with MongoDB connection and response handling

---
+++ auth/lambda/signin/handler.ts
@@ -0,0 +1,53 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { service } from "./signin.services";
+
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+import { dbHelper } from "../shared/repository/db-helper";
+import { internal } from "../shared/http/500";
+
+const secretsManager = new SecretsManagerClient();
+
+async function getMongoUri(): Promise<string> {
+  const res = await secretsManager.send(
+    new GetSecretValueCommand({
+      SecretId: "mongodb/uri",
+    })
+  );
+
+  const secret = JSON.parse(res.SecretString ?? "{}");
+  return secret["MONGO_URI"];
+}
+
+let isConnected = false;
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    if (!isConnected) {
+      const uri = await getMongoUri();
+      await dbHelper.connect(uri);
+      isConnected = true;
+    }
+
+    const data = JSON.parse(event.body || "{}");
+
+    const response = await service(data);
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify(response),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, X-Api-Key",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+      },
+    };
+  } catch (error) {
+    console.error("Error:", error);
+    return internal();
+  }
+};
