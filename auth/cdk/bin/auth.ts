cc942a7ac680cfa312a087ef40a65b753ac92e0e
igorcruzz <igorcruz.dev@gmail.com>
2025-07-13 20:08:40 -0300

    fix: correct import path for AuthStack and update stack name

--- auth/cdk/bin/auth.ts
+++ auth/cdk/bin/auth.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { AuthStack } from '../lib/auth-stack';
+import { AuthStack } from '../lib/stacks/auth-stack';
 
 const app = new cdk.App();
-new AuthStack(app, 'AuthStack');
+new AuthStack(app, 'stack-auth');
