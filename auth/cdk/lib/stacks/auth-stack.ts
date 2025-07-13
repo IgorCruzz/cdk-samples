9eefcbd3cf3fe6040a5abe6e6700b579dfcff5be
igorcruzz <igorcruz.dev@gmail.com>
2025-07-13 20:08:48 -0300

    feat: implement AuthStack with LambdaConstruct for user authentication

---
+++ auth/cdk/lib/stacks/auth-stack.ts
@@ -0,0 +1,12 @@
+import { Stack, StackProps } from 'aws-cdk-lib'; 
+import { Construct } from 'constructs';
+import { LambdaConstruct } from '../constructs/lambda.construct';  
+
+
+export class AuthStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props); 
+
+    new LambdaConstruct(this, 'construct-lambda');
+  }
+}
