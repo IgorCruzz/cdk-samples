10cf0fa9ab5933315d7e7f0813c88c1fc62aec22
igorcruzz <igorcruz.dev@gmail.com>
2025-07-13 21:22:15 -0300

    feat: refactor JWT signing to retrieve secret from AWS Secrets Manager

--- auth/lambda/shared/infra/jwt.ts
+++ auth/lambda/shared/infra/jwt.ts
@@ -1,16 +1,30 @@
+import { GetSecretValueCommand, SecretsManagerClient } from '@aws-sdk/client-secrets-manager';
 import { sign } from 'jsonwebtoken' 
 
+async function getJWTSecret(): Promise<string> {
+  const res = await secretsManager.send(
+    new GetSecretValueCommand({
+      SecretId: "jwt/secret",
+    })
+  );
+
+  const secret = JSON.parse(res.SecretString ?? "{}");
+  return secret["JWT_SECRET"];
+}
+
+const secretsManager = new SecretsManagerClient();
+
 export const jwt = {
-  sign: (payload: object): {
+  sign: async (payload: object): Promise<{
     accessToken: string;
     refreshToken: string;
-  } => {
-    
-    const accessToken = sign(payload, process.env.JWT_SECRET as string, {
+  }> => {
+
+    const accessToken = sign(payload, await getJWTSecret(), {
       expiresIn: '1h',
     });
 
-    const refreshToken = sign(payload, process.env.JWT_SECRET as string, {
+    const refreshToken = sign(payload, await getJWTSecret(), {
       expiresIn: '7d',
     });
 
