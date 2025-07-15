ae5679188d6fda58602bf9d23c6e9ae7f3285d8d
igorcruzz <igorcruz.dev@gmail.com>
2025-07-14 23:01:16 -0300

    feat: add routing configuration with Public and Private routes

---
+++ frontend/app/src/routes/index.tsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Auth from '@/pages/Auth';
+import Home from '@/pages/Home';
+import PrivateLayout from '@/components/AppLayout';
+import PublicLayout from '@/components/PublicLayout'; 
+import {PublicRoute} from './public-route';
+import {PrivateRoute} from './private-route';
+ 
+export const router = createBrowserRouter([  
+  {
+    element: <PublicRoute />, 
+    children: [
+      {
+        element: <PublicLayout />,
+        children: [
+          { path: "/", element: <Auth /> },
+        ],
+      },
+    ],
+    },   
+    {
+    element: <PrivateRoute />, 
+    children: [
+      { 
+        element: <PrivateLayout />,
+        children: [
+          { path: "home", element: <Home /> }, 
+        ],
+      },
+    ],
+  },
+]);
+ 
