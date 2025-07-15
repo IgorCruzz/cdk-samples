dc193a386c9483116dfe72dd3ae66250e31e0219
igorcruzz <igorcruz.dev@gmail.com>
2025-07-14 23:01:19 -0300

    feat: implement PrivateRoute component for protected routing

---
+++ frontend/app/src/routes/private-route.tsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from "react-router-dom"; 
+import { useAuthStore } from '@/store/use-auth';
+
+const isAuthenticated = () => {
+  const getAccessToken = useAuthStore((state) => state.getAccessToken) 
+
+  return Boolean(getAccessToken());
+}; 
+
+export function PrivateRoute() {
+  return isAuthenticated() ? <Outlet /> : <Navigate to="/" replace />;
+} 
+ 
