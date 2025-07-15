f7dbd7133cb6fb066f4738afacf6f7b7fe1cb1ff
igorcruzz <igorcruz.dev@gmail.com>
2025-07-14 23:01:25 -0300

    feat: implement API request and response interceptors for authentication handling

--- frontend/app/src/services/api.ts
+++ frontend/app/src/services/api.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { auth } from './endpoints/auth';
+import { useAuthStore } from '@/store/use-auth';
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -7,4 +9,48 @@ export const api = axios.create({
   }
 })
 
- 
+ api.interceptors.request.use(function (config) {
+  const storage = localStorage.getItem('auth-storage');
+
+  if (storage) {
+    const parsedStorage = JSON.parse(storage);
+
+    if (!parsedStorage.state.accessToken) return config;
+
+    config.headers.Authorization = `Bearer ${parsedStorage.state.accessToken}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const { refreshToken, setAccessToken, setRefreshToken } =
+          useAuthStore.getState();
+
+        if (!refreshToken) return Promise.reject(error);
+
+        const response = await auth.refresh({
+          token: refreshToken,
+        });
+
+        setAccessToken({ accessToken: response.data.accessToken });
+        setRefreshToken({ refreshToken: response.data.refreshToken });
+
+        error.config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+
+        return api(error.config);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
