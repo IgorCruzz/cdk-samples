610abf4330b0b5e88291169ab74984ab5b33f787
igorcruzz <igorcruz.dev@gmail.com>
2025-07-14 23:01:27 -0300

    refactor: replace AWS Cognito integration with custom API for login functionality

--- frontend/app/src/services/endpoints/auth.ts
+++ frontend/app/src/services/endpoints/auth.ts
@@ -1,19 +1,15 @@
-import axios from 'axios'
+import { api } from '../api'
 
 export const auth = {
   login: async (data: { email: string; password: string }) => {
-    return await axios.post(
-      "https://us-east-1vqukuu8e5.auth.us-east-1.amazoncognito.com/oauth2/token",
-      new URLSearchParams({
-        grant_type: "password",
-        client_id: "5jt1ofjdv34b344lbiq1jj5av7",
-        username: data.email,
-        password: data.password,
-    }),
-    {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
+    return await api.post("auth/signin", {
+      email: data.email,
+      password: data.password,
+    })
+  },
+  refresh: async (data: { token: string }) => {
+    return await api.post("auth/refresh", {
+      token: data.token,
     })
   }
 }
