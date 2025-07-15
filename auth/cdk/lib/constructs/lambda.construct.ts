0cb68f674afcfae196334058999adb4c7af2192b
igorcruzz <igorcruz.dev@gmail.com>
2025-07-15 20:47:12 -0300

    fix: add role policy for secrets manager access in refresh token function

--- auth/cdk/lib/constructs/lambda.construct.ts
+++ auth/cdk/lib/constructs/lambda.construct.ts
@@ -118,6 +118,19 @@ export class LambdaConstruct extends Construct {
       stringValue: fn.functionArn,
     })
 
+    const region = Stack.of(this).region;
+    const account = Stack.of(this).account;
+
+    fn.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["secretsmanager:GetSecretValue"],
+        resources: [
+          `arn:aws:secretsmanager:${region}:${account}:secret:mongodb/uri-*`,
+          `arn:aws:secretsmanager:${region}:${account}:secret:jwt/secret-*`,
+        ],
+      })
+    );
+
     return fn;
   }
 }
