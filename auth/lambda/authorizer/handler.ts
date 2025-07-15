e164245854456fb4c6af9722406f4923dbf4811c
igorcruzz <igorcruz.dev@gmail.com>
2025-07-14 23:16:45 -0300

    refactor: update authorizer handler to use APIGatewayTokenAuthorizerEvent and simplify response structure

--- auth/lambda/authorizer/handler.ts
+++ auth/lambda/authorizer/handler.ts
@@ -1,53 +1,41 @@
-import { APIGatewayProxyEvent } from "aws-lambda";
-import { service } from "./authorizer.services";
+import { APIGatewayAuthorizerResult, APIGatewayTokenAuthorizerEvent } from "aws-lambda";
+import { service } from "./authorizer.services";  
 
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager";
-
-import { dbHelper } from "../shared/repository/db-helper";
-import { internal } from "../shared/http/500";
-
-const secretsManager = new SecretsManagerClient();
-
-async function getMongoUri(): Promise<string> {
-  const res = await secretsManager.send(
-    new GetSecretValueCommand({
-      SecretId: "mongodb/uri",
-    })
-  );
-
-  const secret = JSON.parse(res.SecretString ?? "{}");
-  return secret["MONGO_URI"];
-}
-
-let isConnected = false;
-
-export const handler = async (event: APIGatewayProxyEvent) => {
-  try {
-    if (!isConnected) {
-      const uri = await getMongoUri();
-      await dbHelper.connect(uri);
-      isConnected = true;
-    }
-
-    const data = JSON.parse(event.body || "{}");
-
-    const response = await service(data);
+export const handler = async (
+  event: APIGatewayTokenAuthorizerEvent
+): Promise<APIGatewayAuthorizerResult> => {
+  try {  
+    const token = event.authorizationToken 
+    
+    const response = await service({ token });
 
     return {
-      statusCode: 200,
-      body: JSON.stringify(response),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers":
-          "Content-Type, Authorization, X-Api-Key",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
-      },
+      principalId: "user",
+      policyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "execute-api:Invoke",
+            Effect: response.success ? "Allow" : "Deny",
+            Resource: event.methodArn,
+          },
+        ],
+      }
     };
   } catch (error) {
     console.error("Error:", error);
-    return internal();
+    return {
+      principalId: "user",
+      policyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "execute-api:Invoke",
+            Effect: "Deny",
+            Resource: event.methodArn,
+          },
+        ],
+      }
   }
+}
 };
