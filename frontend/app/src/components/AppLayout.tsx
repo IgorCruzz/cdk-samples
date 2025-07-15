61611449218de0ad8963d8ab80e877bee553dea8
igorcruzz <igorcruz.dev@gmail.com>
2025-07-14 23:01:07 -0300

    feat: implement logout functionality in AppLayout component

--- frontend/app/src/components/AppLayout.tsx
+++ frontend/app/src/components/AppLayout.tsx
@@ -1,6 +1,17 @@
+import { Outlet, useNavigate } from "react-router-dom";
 import Logo from '@/assets/logo.png';
+import { Button } from './ui/button'
+import { useAuthStore } from '@/store/use-auth'; 
 
-const AppLayout = ({ children }: { children: React.ReactNode }) => {
+const AppLayout = () => {
+  const navigate = useNavigate();
+  const logout = useAuthStore((state) => state.logout);
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
   return (
   <div className="min-h-screen flex flex-col justify-between">
   <div className="absolute top-0 left-0 w-full h-[4px] rounded-tl-4xl bg-gradient-to-r from-green-500 to-white z-10" />
@@ -8,10 +19,14 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
   <header className="h-[80px] flex items-center justify-between px-6 bg-gradient-to-r from-green-500 to-blue-500">
         <img src={Logo} alt="Logo" height={60} width={60} />
         <span className="text-white font-bold">Bem-vindo</span>
+        <Button 
+        onClick={handleLogout}
+        className="bg-white text-green-500 hover:bg-gray-200">Logout</Button>
+
   </header>
 
   <main className="flex-1 p-8">
-    {children}
+    <Outlet />
   </main>
 
   <footer className="h-[50px] flex items-center justify-center bg-[oklch(15.823%_0.01178_260.646)]">
