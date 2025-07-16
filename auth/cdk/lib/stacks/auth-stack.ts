d7202d9097977a9d9a456b9a2ec7697ef0db7eaf
igorcruzz <igorcruz.dev@gmail.com>
2025-07-15 21:52:33 -0300

    refactor: remove ApiConstruct from AuthStack and related instantiation

--- auth/cdk/lib/stacks/auth-stack.ts
+++ auth/cdk/lib/stacks/auth-stack.ts
@@ -1,16 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib'; 
 import { Construct } from 'constructs';
-import { LambdaConstruct, ApiConstruct } from '../constructs';  
+import { LambdaConstruct } from '../constructs';  
 
 export class AuthStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props); 
 
-    const lambdaContruct = new LambdaConstruct(this, 'construct-lambda');
-
-    new ApiConstruct(this, 'construct-api', {
-       signinFunction: lambdaContruct.signinFunction,
-       refreshTokenFunction: lambdaContruct.refreshTokenFunction
-    });
+    new LambdaConstruct(this, 'construct-lambda'); 
   }
 }
