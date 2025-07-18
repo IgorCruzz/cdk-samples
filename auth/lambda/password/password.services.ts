fe21b79c3050527ab510b93ae3c78876a0e49bd6
igorcruzz <igorcruz.dev@gmail.com>
2025-07-17 23:08:31 -0300

    feat: add password service to handle password change requests with Cognito

---
+++ auth/lambda/password/password.services.ts
@@ -0,0 +1,28 @@
+import { Output } from "../shared/service/output"; 
+import { cognito } from '../shared/infra/cognito'; 
+
+type Input = {
+  email: string; 
+  password: string; 
+  session?: string;
+} 
+
+export const service = async (data: Input): Output<{ 
+  accessToken?: string; 
+  refreshToken?: string;  
+ }>  => {  
+  const { email, password, session } = data; 
+
+  const auth = await cognito.authChallenge(
+    {
+      email,
+      password,
+      session 
+    } )
+
+  if (auth.error) {
+    return { message: auth.error, success: false, data:  null }; 
+  } 
+
+  return { message: "Password changed successfully", success: true, data: null };
+};
