ecf3d360ac3560a6cc2ebdbfb758f675d0a49fde
igorcruzz <igorcruz.dev@gmail.com>
2025-07-17 22:16:15 -0300

    feat: implement Cognito authentication logic with SSM parameter retrieval

---
+++ auth/lambda/shared/infra/cognito.ts
@@ -0,0 +1,54 @@
+import {
+  AuthFlowType,
+  CognitoIdentityProviderClient, 
+  InitiateAuthCommand,
+  InitiateAuthCommandInput,
+  InitiateAuthCommandOutput
+} from "@aws-sdk/client-cognito-identity-provider";
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+
+const cognitoClient = new CognitoIdentityProviderClient({});
+const ssmClient = new SSMClient({});
+
+async function getUserPoolClientId(): Promise<string> {
+  const comando = new GetParameterCommand({
+    Name: "/cognito/user-pool-client-id",
+  });
+
+  const resultado = await ssmClient.send(comando);
+  return resultado.Parameter?.Value || "";
+}
+
+export const cognito = {
+  auth: async ({ email, password }: { email: string; password: string }): Promise<
+  {
+    accessToken: string;
+    refreshToken: string;
+  }
+  | undefined> => {
+    try {
+      const clientId = await getUserPoolClientId();
+
+      const params: InitiateAuthCommandInput = {
+       ClientId: clientId,
+       AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
+       AuthParameters: {
+          USERNAME: email,
+          PASSWORD: password,  
+       }
+    };
+
+    const command = new InitiateAuthCommand(params);
+
+    const res: InitiateAuthCommandOutput = await cognitoClient.send(command);  
+
+    const authResult = res.AuthenticationResult;
+
+    if (!authResult ) return undefined;    
+
+    return { accessToken: authResult.AccessToken!, refreshToken: authResult.RefreshToken! };
+    } catch (error) {
+      return undefined; 
+    }    
+  },
+};
