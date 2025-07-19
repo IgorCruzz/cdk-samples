e6857e275a1764bc5e330c3d7b2ce9d104796cf9
igorcruzz <igorcruz.dev@gmail.com>
2025-07-19 18:22:19 -0300

    feat: add route for Customers page in the router configuration

--- frontend/app/src/routes/index.tsx
+++ frontend/app/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Auth from '@/pages/Auth';
 import Home from '@/pages/Home';
 import File from '@/pages/File';
+import Customers from '@/pages/Customers';
 import PrivateLayout from '@/components/AppLayout';
 import PublicLayout from '@/components/PublicLayout'; 
 import {PublicRoute} from './public-route';
@@ -27,6 +28,7 @@ export const router = createBrowserRouter([
         children: [
           { path: "home", element: <Home /> }, 
           { path: "upload", element: <File /> },
+          { path: "customers", element: <Customers /> },
         ],
       },
     ],
