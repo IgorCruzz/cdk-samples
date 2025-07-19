c8478fec20503e21f6cb5790f0defc4bded2cd82
igorcruzz <igorcruz.dev@gmail.com>
2025-07-19 18:06:54 -0300

    fix: remove welcome message from Sidebar component

--- frontend/app/src/components/Sidebar.tsx
+++ frontend/app/src/components/Sidebar.tsx
@@ -25,7 +25,6 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarHeader className="items-center justify-center gap-5">
         <img src={Logo} alt="Logo" width={100} />
-        <p>Bem vindo</p>
       </SidebarHeader>
 
       <SidebarContent>
