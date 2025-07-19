37e1ac5fca986085f0e32c21c19aea1ce4829032
igorcruzz <igorcruz.dev@gmail.com>
2025-07-19 13:21:38 -0300

    refactor: extract authentication check into a separate function in PublicRoute

--- frontend/app/src/routes/public-route.tsx
+++ frontend/app/src/routes/public-route.tsx
@@ -1,9 +1,11 @@
 import { useAuthStore } from "@/store/use-auth";
 import { Navigate, Outlet } from "react-router-dom";
 
-export function PublicRoute() {
+const isNotAuthenticated = () => {
   const getAccessToken = useAuthStore((state) => state.getAccessToken);
-  const isAuth = Boolean(getAccessToken());
+  return !getAccessToken();
+}
 
-  return isAuth ? <Navigate to="/home" replace /> : <Outlet />;
+export function PublicRoute() {
+  return !isNotAuthenticated() ? <Navigate to="/home" replace /> : <Outlet />;
 } 
