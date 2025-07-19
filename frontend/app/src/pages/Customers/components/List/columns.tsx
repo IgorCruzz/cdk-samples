78fc86cc3c9617db47475e2b8cae5be0725022a1
igorcruzz <igorcruz.dev@gmail.com>
2025-07-19 18:21:39 -0300

    feat: add columns definition for File component in List

---
+++ frontend/app/src/pages/Customers/components/List/columns.tsx
@@ -0,0 +1,57 @@
+import { formatBytes } from "@/utils/formatByes"
+import { type ColumnDef } from "@tanstack/react-table"
+import { CircleEllipsis, ThumbsDown, ThumbsUp } from "lucide-react"
+
+export type File = {
+  size: number
+  status: string 
+  key: string
+  successLines: number
+  failedLines: number
+}
+
+export const columns: ColumnDef<File>[] = [
+  {
+    accessorKey: "key",
+    header: "Key",
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+    cell: (info) => `${formatBytes(info.getValue() as number)}`,
+  },
+  {
+    accessorKey: "successLines",
+    header: "Success Lines",
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: "failedLines",
+    header: "Failed Lines",
+    cell: (info) => info.getValue(),
+  },
+{
+  accessorKey: "status",
+  header: "Status",  
+  cell: (info) => {
+    const rowData = info.row.original;
+
+    const successLines = rowData.successLines; 
+
+    return (
+    <>
+    {info.getValue() === 'COMPLETED' && (
+      <>
+       {successLines > 0 ? <ThumbsUp color="green" /> : <ThumbsDown color="red" />}  
+      </>
+    )}
+
+    {info.getValue() === 'FAILED' && (<ThumbsDown color="red" />)}
+
+    {info.getValue() === 'PROCESSING' && (<CircleEllipsis />)}
+    </>     
+  )
+  }
+}
+]
