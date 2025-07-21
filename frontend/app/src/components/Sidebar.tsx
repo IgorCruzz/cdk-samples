d96e40092881397f320c8401f0ce64746470cf20
igorcruzz <igorcruz.dev@gmail.com>
2025-07-21 20:18:03 -0300

    refactor(Sidebar): streamline sidebar structure and remove unused components

--- frontend/app/src/components/Sidebar.tsx
+++ frontend/app/src/components/Sidebar.tsx
@@ -1,74 +1,84 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
-  SidebarHeader,
-} from "@/components/ui/sidebar";
-import Logo from '@/assets/logo.png';
-import { useAuthStore } from "@/store/use-auth";
-import { Link, useLocation } from "react-router-dom";
-import { Button } from "./ui/button";
-import { House, PersonStanding, Upload } from "lucide-react";
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { Link } from "react-router-dom"
+
+ 
+const applicationRoutes = [
+  {
+    title: "Home",
+    url: "/home",
+    icon: Home,
+  },
+  {
+    title: "Upload",
+    url: "/upload",
+    icon: Inbox,
+  },
+  {
+    title: "Customers",
+    url: "/customers",
+    icon: Calendar,
+  }, 
+]
+
+const administratorRoutes =[
+  {
+    title: "Users",
+    url: "/users",
+    icon: Home,
+  }, 
+]
 
 export function AppSidebar() {
-  const location = useLocation();
-  const logout = useAuthStore((state) => state.logout);
-
-  const handleLogout = () => {
-    logout();
-  };
-
-  const isActive = (path: string) => location.pathname === path;
-
-  const linkClass = (path: string) =>
-    `flex items-center gap-2 text-gray-200 p-2 rounded transition-colors duration-300 ${
-      isActive(path)
-        ? 'bg-gradient-to-r from-green-300/70 to-green-500/70 hover:from-green-600/70 hover:to-green-800/70'
-        : 'hover:bg-green-900/30'
-    }`;
-
   return (
     <Sidebar>
-      <SidebarHeader>
-        <img src={Logo} alt="Logo" width={100} />
-      </SidebarHeader>
-
       <SidebarContent>
         <SidebarGroup>
-          <ul className="w-full space-y-2 text-sm">
-            <li>
-              <Link to="/home" className={linkClass('/home')}>
-                <House /> Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/upload" className={linkClass('/upload')}>
-                <Upload /> Upload
-              </Link>
-            </li>
-            <li>
-              <Link to="/customers" className={linkClass('/customers')}>
-                <PersonStanding /> Customers
-              </Link>
-            </li>
-            <li>
-              <Link to="/users" className={linkClass('/users')}>
-                <PersonStanding /> Users
-              </Link>
-            </li>
-          </ul>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {applicationRoutes.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
         </SidebarGroup>
-      </SidebarContent>
 
-      <SidebarFooter>
-        <Button
-          onClick={handleLogout}
-          className="bg-transparent w-full text-gray-200 p-2 rounded"
-        >
-          Logout
-        </Button>
-      </SidebarFooter>
+         <SidebarGroup>
+          <SidebarGroupLabel>Administrator</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {administratorRoutes.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link to={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup> 
+      </SidebarContent>
     </Sidebar>
-  );
+  )
 }
