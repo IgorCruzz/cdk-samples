c97ced3854f6a40b53d0d639152d0c4198597f4d
igorcruzz <igorcruz.dev@gmail.com>
2025-07-21 20:05:45 -0300

    feat(users): add AddOrUpdate component to Users page for user management

--- frontend/app/src/pages/Users/index.tsx
+++ frontend/app/src/pages/Users/index.tsx
@@ -1,10 +1,17 @@
 
-import { List } from './components'; 
+import { List, AddOrUpdate } from './components'; 
 
 export default function Users() { 
-  return (
-    <div className="flex flex-col gap-8">  
-        <List /> 
-    </div>
+  return ( 
+        <div className="flex flex-col gap-8">
+          <div className="flex justify-end items-center">  
+            <AddOrUpdate /> 
+          </div>
+    
+          <div className="w-full">
+            <List />
+          </div>
+        </div>
+       
   );
 }
