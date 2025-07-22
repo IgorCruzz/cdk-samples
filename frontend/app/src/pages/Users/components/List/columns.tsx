9a41f2bca949a8bfb6c20e706c2655508e731bfd
igorcruzz <igorcruz.dev@gmail.com>
2025-07-21 21:52:31 -0300

    fix(Columns): update type definition to use UserInput for better type safety

--- frontend/app/src/pages/Users/components/List/columns.tsx
+++ frontend/app/src/pages/Users/components/List/columns.tsx
@@ -2,14 +2,10 @@ import { Button } from "@/components/ui/button";
 import { type ColumnDef } from "@tanstack/react-table"
 import { Delete } from 'lucide-react';
 import { AddOrUpdate } from '../AddOrUpdate'
+import { UserInput } from '@/schemas/users'
+ 
 
-export type Customers = {
-  id: string;
-  name: string;
-  email: string;
-};
-
-export const columns: ColumnDef<Customers>[] = [ 
+export const columns: ColumnDef<UserInput>[] = [ 
   {
     accessorKey: "name",
     header: "Name",
@@ -23,7 +19,7 @@ export const columns: ColumnDef<Customers>[] = [
   {
     id: 'actions',
     header: 'Actions',
-    cell: ({ row }) => { 
+    cell: ({ row }) => {  
       return (
       <div className="flex items-center gap-2"> 
         <AddOrUpdate type="update" user={row.original}/> 
