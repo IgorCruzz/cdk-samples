2606a518f04c50ccdf66f7d67627e2b1890a5685
igorcruzz <igorcruz.dev@gmail.com>
2025-07-21 23:39:08 -0300

    feat(user.repository): implement user repository with findBySub method

---
+++ sheet-parse/lambda/shared/repository/user.repository.ts
@@ -0,0 +1,25 @@
+import { dbHelper } from "./db-helper";
+
+export type Users = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  sub?: string;
+};
+
+export interface IUserRepository {
+  findBySub(id: string): Promise<Users | null>;
+}
+
+export const userRepository: IUserRepository = {
+  async findBySub(id: string): Promise<Users | null> {
+    const users = dbHelper.getCollection("users");
+
+    const user = await users.findOne({ sub: id });
+
+    return user && dbHelper.map(user);
+  },
+};
