1a92e6e0158dc5e8977c3c92e1117b8b2efa3dfc
igorcruzz <igorcruz.dev@gmail.com>
2025-07-22 20:49:50 -0300

    refactor(sheet-parse): simplify LambdaConstruct initialization and remove redundant tagging

--- sheet-parse/cdk/lib/stacks/sheet-parse.stack.ts
+++ sheet-parse/cdk/lib/stacks/sheet-parse.stack.ts
@@ -8,26 +8,8 @@ export class SheetParseStack extends Stack {
 
     const s3Construct = new S3Construct(this, "construct-s3");
 
-    const lambdaConstruct = new LambdaConstruct(this, "construct-lambda", {
+    new LambdaConstruct(this, "construct-lambda", {
       s3Construct,
     });
-
-    Tags.of(s3Construct.bucket).add("Project", "sheet-parse");
-    Tags.of(s3Construct.bucket).add("Name", "sheet-parse-bucket");
-
-    Tags.of(lambdaConstruct.generatePreSignedUrlFunction).add(
-      "Project",
-      "sheet-parse"
-    );
-    Tags.of(lambdaConstruct.generatePreSignedUrlFunction).add(
-      "Name",
-      "generate-pre-signed-url-function"
-    );
-
-    Tags.of(lambdaConstruct.extractDataFunction).add("Project", "sheet-parse");
-    Tags.of(lambdaConstruct.extractDataFunction).add(
-      "Name",
-      "extract-data-function"
-    );
   }
 }
