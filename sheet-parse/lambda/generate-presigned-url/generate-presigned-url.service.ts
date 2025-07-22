d5334121be730846963735ee3e0849f3cf4273d9
igorcruzz <igorcruz.dev@gmail.com>
2025-07-21 23:38:58 -0300

    fix(generate-presigned-url): add user validation and save archive entry with 'PENDING' status

--- sheet-parse/lambda/generate-presigned-url/generate-presigned-url.service.ts
+++ sheet-parse/lambda/generate-presigned-url/generate-presigned-url.service.ts
@@ -1,11 +1,29 @@
 import { s3 } from "../shared/infra/s3";
+import { archiveRepository } from "../shared/repository/archive.repository";
+import { userRepository } from "../shared/repository/user.repository";
 
-export const service = async (): Promise<{
+export const service = async (
+  userId: string
+): Promise<{
   url: string;
   key: string;
 }> => {
   const preSignedUrl = await s3.createPresignedUrl({
     bucket: process.env.BUCKET_NAME as string,
+  });
+
+  const findUser = await userRepository.findBySub(userId);
+
+  if (!findUser) {
+    throw new Error("User not found");
+  }
+
+  await archiveRepository.save({
+    key: preSignedUrl.key,
+    size: 0,
+    message: ``,
+    status: "PENDING",
+    userId: findUser.id,
   });
 
   return {
