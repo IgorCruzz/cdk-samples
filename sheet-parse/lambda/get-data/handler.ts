1c6a43cd1078c6967190a73d6dda00b451efc668
igorcruzz <igorcruz.dev@gmail.com>
2025-07-22 20:48:00 -0300

    feat(handler): implement API Gateway handler for data retrieval with MongoDB connection

---
+++ sheet-parse/lambda/get-data/handler.ts
@@ -0,0 +1,53 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { service } from "./get-data.services";
+
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+import { dbHelper } from "../shared/repository/db-helper";
+
+const secretsManager = new SecretsManagerClient();
+
+async function getMongoUri(): Promise<string> {
+  const res = await secretsManager.send(
+    new GetSecretValueCommand({
+      SecretId: "mongodb/uri",
+    })
+  );
+
+  const secret = JSON.parse(res.SecretString ?? "{}");
+  return secret["MONGO_URI"];
+}
+
+let isConnected = false;
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    if (!isConnected) {
+      const uri = await getMongoUri();
+      await dbHelper.connect(uri);
+      isConnected = true;
+    }
+
+    const page = parseInt(event.queryStringParameters?.page || "1");
+
+    const limit = parseInt(event.queryStringParameters?.limit || "20");
+
+    const { archiveId } = event.pathParameters as { archiveId: string };
+
+    const files = await service({ page, limit, archiveId });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(files),
+    };
+  } catch (error) {
+    console.error("Error in handler:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
+};
