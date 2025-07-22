2810e4c8c23fbd35d1020ed3cc1a9d320503018d
igorcruzz <igorcruz.dev@gmail.com>
2025-07-21 23:39:01 -0300

    fix(generate-presigned-url): refactor handler to include MongoDB connection and user ID extraction

--- sheet-parse/lambda/generate-presigned-url/handler.ts
+++ sheet-parse/lambda/generate-presigned-url/handler.ts
@@ -1,8 +1,39 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
 import { service } from "./generate-presigned-url.service";
 
-export const handler = async () => {
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+import { dbHelper } from "../shared/repository/db-helper";
+
+const secretsManager = new SecretsManagerClient();
+
+async function getMongoUri(): Promise<string> {
+  const res = await secretsManager.send(
+    new GetSecretValueCommand({
+      SecretId: "mongodb/uri",
+    })
+  );
+
+  const secret = JSON.parse(res.SecretString ?? "{}");
+  return secret["MONGO_URI"];
+}
+
+let isConnected = false;
+
+export const handler = async (event: APIGatewayProxyEvent) => {
   try {
-    const preSignedUrl = await service();
+    if (!isConnected) {
+      const uri = await getMongoUri();
+      await dbHelper.connect(uri);
+      isConnected = true;
+    }
+
+    const userId = event.requestContext.authorizer?.jwt.claims?.sub;
+
+    const preSignedUrl = await service(userId);
 
     return {
       statusCode: 200,
