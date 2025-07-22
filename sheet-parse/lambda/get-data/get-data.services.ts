9f3622f024a9d4a963de2368b76405efa0017c87
igorcruzz <igorcruz.dev@gmail.com>
2025-07-22 20:47:53 -0300

    feat(get-data-service): implement data retrieval service with pagination support

---
+++ sheet-parse/lambda/get-data/get-data.services.ts
@@ -0,0 +1,25 @@
+import { dataRepository } from "../shared/repository/data.repository";
+
+export const service = async ({
+  page,
+  limit,
+  archiveId,
+}: {
+  page: number;
+  limit: number;
+  archiveId: string;
+}): Promise<{
+  itens: unknown[];
+  count: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+}> => {
+  const data = await dataRepository.get({
+    page,
+    limit,
+    archiveId,
+  });
+
+  return data;
+};
