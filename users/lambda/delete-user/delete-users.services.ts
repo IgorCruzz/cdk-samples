e78a5bcbe2cb5cd431031fbea796cf62aa6c9e60
igorcruzz <igorcruz.dev@gmail.com>
2025-07-22 23:34:34 -0300

    fix(delete-user): remove user from Cognito before deleting from repository

--- users/lambda/delete-user/delete-users.services.ts
+++ users/lambda/delete-user/delete-users.services.ts
@@ -1,4 +1,5 @@
 import { userRepository } from "../shared/repository/user.repository";
+import { cognito } from "../shared/infra/cognito";
 import { Output } from "../shared/service/output";
 
 export const service = async ({ id }: { id: string }): Output => {
@@ -7,6 +8,8 @@ export const service = async ({ id }: { id: string }): Output => {
   if (!verifyUser) {
     return { message: "User not found", success: false, data: null };
   }
+
+  await cognito.removeUser(verifyUser.email);
 
   await userRepository.delete({
     id,
