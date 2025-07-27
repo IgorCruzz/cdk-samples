a34d98c95bc273ba753ddb594b4e6e4694e7545f
igorcruzz <igorcruz.dev@gmail.com>
2025-07-27 20:57:19 -0300

    fix(lambda): improve token retrieval logic and enhance output structure in OAuth2 service

--- auth/lambda/oauth2/oauth2.services.ts
+++ auth/lambda/oauth2/oauth2.services.ts
@@ -1,13 +1,35 @@
 import { Output } from "../shared/service/output";
+import { cognito } from "../shared/infra/cognito";
 
 type Input = {
   code: string;
 };
 
-export const service = async (data: Input): Output<any> => {
-  const { code } = data;
+export const service = async ({
+  code,
+}: Input): Output<{
+  accessToken: string | null;
+  refreshToken: string | null;
+}> => {
+  const { accessToken, refreshToken } = await cognito.getToken({ code });
 
-  console.log({ code });
+  if (!accessToken || !refreshToken) {
+    return {
+      message: "Failed to retrieve tokens",
+      success: false,
+      data: {
+        accessToken: null,
+        refreshToken: null,
+      },
+    };
+  }
 
-  return { message: "Login successful", success: true, data: {} };
+  return {
+    message: "Login successful",
+    success: true,
+    data: {
+      accessToken,
+      refreshToken,
+    },
+  };
 };
