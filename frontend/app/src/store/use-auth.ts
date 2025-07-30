c1cebc80d7e80de305e435c9b28bec2dbf0a052a
igorcruzz <igorcruz.dev@gmail.com>
2025-07-30 20:32:28 -0300

    refactor(auth): consolidate token handling into setTokens function

--- frontend/app/src/store/use-auth.ts
+++ frontend/app/src/store/use-auth.ts
@@ -3,24 +3,26 @@ import { persist } from 'zustand/middleware';
 
 interface AuthState { 
   accessToken: string | null;
-  refreshToken: string | null;  
-  setAccessToken: (payload: { accessToken: string }) => void;
-  setRefreshToken: (payload: { refreshToken: string }) => void;
+  refreshToken: string | null;   
   getAccessToken: () => string | null;
   logout: () => void;
+  setTokens: (payload: { accessToken: string, refreshToken: string }) => void;
 }
 
 export const useAuthStore = create<AuthState>()(
   persist(
     (set, get) => ({   
       accessToken: null,
-      refreshToken: null,  
-      setAccessToken: ({ accessToken }) =>
-        set({
-          accessToken, 
-        }),
-      setRefreshToken: ({ refreshToken }) => set({ refreshToken }),
+      refreshToken: null,   
       getAccessToken: () => get().accessToken,   
+      setTokens: ({ accessToken, refreshToken }) => {
+        set({
+          accessToken,
+          refreshToken,
+        });
+
+        window.location.href = '/home';  
+      },
       logout: () => {
         window.location.href = '/'
 
