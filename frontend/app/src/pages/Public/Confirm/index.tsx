f7abb529af0a76427f82b7986910c6390375357a
igorcruzz <igorcruz.dev@gmail.com>
2025-07-31 00:19:45 -0300

    feat(confirm): add Confirm component for user code verification

---
+++ frontend/app/src/pages/Public/Confirm/index.tsx
@@ -0,0 +1,66 @@
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from '@/components/ui/input'; 
+import {  confirmInput, confirmSchema } from "@/schemas/confirm";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import { toast } from "sonner";
+import { auth } from '@/services/endpoints/auth'; 
+import { useNavigate } from "react-router-dom";
+ 
+export default function Confirm() { 
+  const navigate = useNavigate();
+ 
+  const {isPending, mutateAsync} = useMutation({
+    mutationFn: async (input: confirmInput) => {
+      const { data } = await auth.confirm(input);
+      return data;
+    }, 
+  }); 
+
+  const form = useForm<{ code: string }>({
+    resolver: zodResolver(confirmSchema), 
+  }); 
+
+  const onSubmit = async ({ code }: confirmInput) => {
+    try {
+      const { data  } = await mutateAsync({ code }); 
+      
+      toast.success(data.message); 
+      
+      navigate('/login');  
+
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+      }
+      console.error(error);  
+    }   
+  }; 
+
+  return (
+     <Card className="w-full h-full flex flex-col items-center justify-center p-6">
+        <h1 className="text-2xl font-bold">Confirm</h1>
+        <CardContent className="w-full">
+          <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-8">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">Code</label>
+              <Input
+                {...form.register("code")}
+                type="code"  
+                required
+              />
+            </div>
+            <Button type="submit" className="w-full" disabled={isPending}>
+              {isPending ? "Logging in..." : "Confirm"}
+            </Button> 
+          </form>
+          </Form> 
+        </CardContent>
+    </Card>    
+  );
+}
