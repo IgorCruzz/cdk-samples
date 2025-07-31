61400f27c3c87f9c327e1772d783e2fec0834332
igorcruzz <igorcruz.dev@gmail.com>
2025-07-31 00:46:42 -0300

    feat(confirm): add handler for account confirmation service

---
+++ auth/lambda/confirm/handler.ts
@@ -0,0 +1,26 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { service } from "./confirm.services";
+import { internal } from "../_shared/http/500";
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    const data = JSON.parse(event.body || "{}");
+
+    const response = await service(data);
+
+    if (!response.success) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(response),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    console.error("Error:", error);
+    return internal();
+  }
+};
