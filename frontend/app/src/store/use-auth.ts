1d2473fc1ce417a57727c0565ec165e4ac49646f
igorcruzz <igorcruz.dev@gmail.com>
2025-08-02 12:37:16 -0300

    refactor(Auth): implement getUser function to decode idToken for user retrieval

--- frontend/app/src/store/use-auth.ts
+++ frontend/app/src/store/use-auth.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware'; 
+import { jwtDecode } from 'jwt-decode'
 
 interface AuthState { 
   accessToken: string | null;
@@ -8,21 +9,33 @@ interface AuthState {
   getAccessToken: () => string | null;
   logout: () => void;
   setTokens: (payload: { accessToken: string, refreshToken: string, idToken: string }) => void;
+  getUser: any
 }
 
 export const useAuthStore = create<AuthState>()(
   persist(
-    (set, get) => ({   
+    (set, get) => ({    
       accessToken: null,
       refreshToken: null,   
       idToken: null,
+      getUser: () => {
+
+        const idToken = get().idToken;
+
+        if (!idToken) {
+          return null;
+        }
+
+        const user =   jwtDecode(idToken);
+        return user;
+      },
       getAccessToken: () => get().accessToken,   
-      setTokens: ({ accessToken, refreshToken, idToken }) => {
+      setTokens:   ({ accessToken, refreshToken, idToken }) => {
         set({
           accessToken,
           refreshToken,
-          idToken
-        });
+          idToken, 
+        }); 
 
         window.location.href = '/home';  
       },
