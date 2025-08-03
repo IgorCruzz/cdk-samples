3bcc7322417f44b9d777bf34e7626f686cac26d7
igorcruzz <igorcruz.dev@gmail.com>
2025-08-03 19:29:45 -0300

    refactor(handler): streamline Mongo URI retrieval by utilizing the existing secret module

--- sheet-parse/lambda/generate-presigned-url/handler.ts
+++ sheet-parse/lambda/generate-presigned-url/handler.ts
@@ -1,32 +1,15 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { service } from "./generate-presigned-url.service";
 
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager";
-
 import { dbHelper } from "../_shared/repository/db-helper";
-
-const secretsManager = new SecretsManagerClient();
-
-async function getMongoUri(): Promise<string> {
-  const res = await secretsManager.send(
-    new GetSecretValueCommand({
-      SecretId: "mongodb/uri",
-    })
-  );
-
-  const secret = JSON.parse(res.SecretString ?? "{}");
-  return secret["MONGO_URI"];
-}
+import { secret } from "../_shared/infra/secret";
 
 let isConnected = false;
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   try {
     if (!isConnected) {
-      const uri = await getMongoUri();
+      const uri = await secret.getMongoUri();
       await dbHelper.connect(uri);
       isConnected = true;
     }
