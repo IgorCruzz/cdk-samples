b60d33db0dd48cc0ad2634d62bffa0d2fa29cf7b
igorcruzz <igorcruz.dev@gmail.com>
2025-08-03 20:47:20 -0300

    feat(vite): add ESLint plugin to Vite configuration for improved linting

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -2,10 +2,29 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import eslint from 'vite-plugin-eslint';
+ 
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ export default defineConfig({
+  plugins: [
+    react(), 
+    tailwindcss(),  
+    eslint()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
