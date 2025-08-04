a3d7a9bc0fd38272f0e5e7af4ba6f5e5423304cb
igorcruzz <igorcruz.dev@gmail.com>
2025-08-04 20:43:39 -0300

    feat: add Brevo API key retrieval using AWS Secrets Manager

---
+++ notification/lambda/_shared/infra/secret.ts
@@ -0,0 +1,19 @@
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const secretsManager = new SecretsManagerClient({});
+
+export const secret = {
+  async getBrevoApiKey(): Promise<string> {
+    const res = await secretsManager.send(
+      new GetSecretValueCommand({
+        SecretId: "brevo/api/key",
+      })
+    );
+
+    const secret = JSON.parse(res.SecretString ?? "{}");
+    return secret["BREVO_API_KEY"];
+  },
+};
