075059ab7449db4ba89ead171d9e25934bd1410b
igorcruzz <igorcruz.dev@gmail.com>
2025-08-03 22:47:21 -0300

    fix(stack): rename SheetParseStack to CsvParseStack for consistency

--- csv-parse/cdk/bin/sheet-parse.ts
+++ csv-parse/cdk/bin/sheet-parse.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { SheetParseStack } from "../lib/stacks/sheet-parse.stack";
+import { CsvParseStack } from "../lib/stacks/sheet-parse.stack";
 
 const app = new cdk.App();
-new SheetParseStack(app, "stack-sheet-parse", {
+new CsvParseStack(app, "stack-sheet-parse", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
