451039c813c239115c0926d2ffc2b5e7a27b9d21
igorcruzz <igorcruz.dev@gmail.com>
2025-08-03 22:47:25 -0300

    fix(stack): rename SheetParseStack to CsvParseStack for consistency

--- csv-parse/cdk/lib/stacks/sheet-parse.stack.ts
+++ csv-parse/cdk/lib/stacks/sheet-parse.stack.ts
@@ -2,7 +2,7 @@ import { Stack, StackProps, Tags } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { S3Construct, LambdaConstruct } from "../constructs";
 
-export class SheetParseStack extends Stack {
+export class CsvParseStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
