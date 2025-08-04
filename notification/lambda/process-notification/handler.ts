5896497508aa917aa7165754960e347b0c72bfeb
igorcruzz <igorcruz.dev@gmail.com>
2025-08-04 20:57:39 -0300

    fix: add duplicate console log for event in handler

--- notification/lambda/process-notification/handler.ts
+++ notification/lambda/process-notification/handler.ts
@@ -4,5 +4,7 @@ import { service } from "./notifier-process.service";
 export const handler = async (event: SQSEvent) => {
   const { Records } = event;
 
+  console.log({ event });
+
   return await service({ records: Records });
 };
