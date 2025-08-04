14ab9497cbf71f014e7e794eb13a04d45065de21
igorcruzz <igorcruz.dev@gmail.com>
2025-08-03 22:49:10 -0300

    feat: implement CsvParseStack for handling CSV parsing in the CDK

---
+++ csv-parse/cdk/lib/stacks/csv-parse.stack.ts
@@ -0,0 +1,15 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { S3Construct, LambdaConstruct } from "../constructs";
+
+export class CsvParseStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const s3Construct = new S3Construct(this, "construct-s3");
+
+    new LambdaConstruct(this, "construct-lambda", {
+      s3Construct,
+    });
+  }
+}
