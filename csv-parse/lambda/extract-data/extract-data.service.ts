2e7530fa2e08bc5bf1c9d97e3b171c59b32928c4
igorcruzz <igorcruz.dev@gmail.com>
2025-08-05 20:57:57 -0300

    refactor: remove unused actualDate import and update createdAt to use new Date()

--- csv-parse/lambda/extract-data/extract-data.service.ts
+++ csv-parse/lambda/extract-data/extract-data.service.ts
@@ -6,7 +6,6 @@ import { dataRepository } from "../_shared/repository/data.repository";
 import { sendNotification } from "../_shared/infra/send-notification";
 import { normalizeRow } from "../_shared/utils/normalize.util";
 import { ObjectId } from "mongodb";
-import { actualDate } from "../_shared/utils/locale-date.util";
 
 export const service = async ({
   s3Record,
@@ -45,7 +44,7 @@ export const service = async ({
         const data = {
           ...normalizedRow,
           archiveId: new ObjectId(file?.id),
-          createdAt: actualDate(),
+          createdAt: new Date(),
         };
 
         chunk.push(data);
