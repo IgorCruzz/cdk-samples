e1cecb2b5ae7b67d2497fabcb58247b53d25352b
igorcruzz <igorcruz.dev@gmail.com>
2025-08-05 20:19:00 -0300

    refactor: remove unused Users route from router configuration

--- frontend/app/src/routes/index.tsx
+++ frontend/app/src/routes/index.tsx
@@ -2,8 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import SingIn from '@/pages/Public/Singin';
 import Signup from '@/pages/Public/Signup';
 import Home from '@/pages/Private/Home';
-import Mocks from '@/pages/Private/Mocks';
-import Users from '@/pages/Private/Users'; 
+import Mocks from '@/pages/Private/Mocks'; 
 import Confirm from '@/pages/Public/Confirm';
 import Redirect from '@/pages/Public/Redirect';
 import PrivateLayout from '@/components/AppLayout';
@@ -34,7 +33,6 @@ export const router = createBrowserRouter([
         children: [
           { path: "home", element: <Home /> }, 
           { path: "mocks", element: <Mocks /> }, 
-          { path: "users", element: <Users /> }, 
         ],
       },
     ],
