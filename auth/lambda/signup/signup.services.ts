2034e14391ad87d68a52386e878bbf8b8f75bbc0
igorcruzz <igorcruz.dev@gmail.com>
2025-08-06 19:59:49 -0300

    refactor: update userRepository.findByEmail and save method calls for improved clarity

--- auth/lambda/signup/signup.services.ts
+++ auth/lambda/signup/signup.services.ts
@@ -5,14 +5,18 @@ import { cognito } from "../_shared/infra/cognito";
 type SignUpInput = Users;
 
 export const service = async (data: SignUpInput): Output => {
-  const existingUser = await userRepository.findByEmail(data.email);
+  const existingUser = await userRepository.findByEmail({ email: data.email });
   if (existingUser) {
     return { message: "Email already exists", success: false, data: null };
   }
 
   const authUser = await cognito.signUp(data.email, data.password);
 
-  await userRepository.save({ ...data, sub: authUser.UserSub });
+  await userRepository.save({
+    ...data,
+    sub: authUser.UserSub,
+    provider: "cognito",
+  });
 
   return { message: "User created successfully", success: true, data: null };
 };
