c8b8b31d691f85b394a4920a7ac5aa629c6ff69a
igorcruzz <igorcruz.dev@gmail.com>
2025-08-06 20:06:23 -0300

    test: add jwt decode test case in OAuth2 service tests

--- auth/lambda/_tests/oauth2/oauth2.test.ts
+++ auth/lambda/_tests/oauth2/oauth2.test.ts
@@ -1,9 +1,24 @@
 import { service } from "../../oauth2/oauth2.services";
 import { cognito } from "../../_shared/infra/cognito";
+import { jwt } from "../../_shared/infra/jwt";
+import { userRepository } from "../../_shared/repository/user.repository";
 
 jest.mock("../../_shared/infra/cognito", () => ({
   cognito: {
     getToken: jest.fn(),
+  },
+}));
+
+jest.mock("../../_shared/infra/jwt", () => ({
+  jwt: {
+    decode: jest.fn(),
+  },
+}));
+
+jest.mock("../../_shared/repository/user.repository", () => ({
+  userRepository: {
+    findByEmail: jest.fn(),
+    save: jest.fn(),
   },
 }));
 
@@ -26,6 +41,22 @@ describe("OAuth2 Service", () => {
     expect(cognito.getToken).toHaveBeenCalledWith({
       code: "test-code",
     });
+  });
+
+  it("should be able to call decoded", async () => {
+    (jwt.decode as jest.Mock).mockResolvedValue({ email: "test@example.com" });
+
+    (cognito.getToken as jest.Mock).mockResolvedValue({
+      accessToken: "mockAccessToken",
+      refreshToken: "mockRefreshToken",
+      idToken: "mockIdToken",
+    });
+
+    await service({
+      code: "test-code",
+    });
+
+    expect(jwt.decode).toHaveBeenCalledWith({ token: "mockIdToken" });
   });
 
   it("should return success message and tokens on successful login", async () => {
