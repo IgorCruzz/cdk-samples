64e52327f38ca624cbfdfe020324b4c1bef9862b
igorcruzz <igorcruz.dev@gmail.com>
2025-08-07 22:58:35 -0300

    test: add unit tests for Create Users Service to verify user repository interactions

--- users/lambda/create-user/create-users.test.ts
+++ users/lambda/create-user/create-users.test.ts
@@ -1,4 +1,33 @@
 import { service } from "./create-users.services";
+import { cognito } from "../_shared/infra/cognito";
+import { userRepository } from "../_shared/repository/user.repository";
+
+jest.mock("../_shared/infra/cognito", () => ({
+  cognito: {
+    removeUser: jest.fn(),
+    createUser: jest.fn().mockResolvedValue({
+      UserConfirmed: false,
+      CodeDeliveryDetails: {
+        AttributeName: "email",
+        DeliveryMedium: "EMAIL",
+        Destination: "email",
+      },
+    }),
+  },
+}));
+jest.mock("../_shared/repository/user.repository", () => ({
+  userRepository: {
+    findByEmail: jest.fn().mockResolvedValue([
+      {
+        id: "1",
+        name: "User One",
+        email: "user1@example.com",
+        password: "hashedpassword",
+        createdAt: new Date(),
+      },
+    ]),
+  },
+}));
 
 describe("Create Users Service", () => {
   beforeEach(() => {
@@ -7,5 +36,17 @@ describe("Create Users Service", () => {
 
   it("should be defined", async () => {
     expect(service).toBeDefined();
+  });
+
+  it("should be able to call findByEmail", async () => {
+    await service({
+      email: "user1@example.com",
+      password: "password",
+      name: "Name",
+    });
+
+    expect(userRepository.findByEmail).toHaveBeenCalledWith(
+      "user1@example.com"
+    );
   });
 });
