0b4ed6a4358e3def63aa5994780541991405f0a3
igorcruzz <igorcruz.dev@gmail.com>
2025-08-07 22:49:02 -0300

    test: add unit tests for Update Users Service to verify service definition

---
+++ users/lambda/update-user/update-users.test.ts
@@ -0,0 +1,11 @@
+import { service } from "./update-users.services";
+
+describe("Update Users Service", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should be defined", async () => {
+    expect(service).toBeDefined();
+  });
+});
