f146e603c2d773f8c7d2a884eca75149bbd5bd40
igorcruzz <igorcruz.dev@gmail.com>
2025-08-08 20:45:41 -0300

    test: refactor Delete Users Service tests to use a request object for consistency

--- users/lambda/delete-user/delete-users.test.ts
+++ users/lambda/delete-user/delete-users.test.ts
@@ -1,3 +1,4 @@
+import { cognito } from "../_shared/infra/cognito";
 import { userRepository } from "../_shared/repository/user.repository";
 import { service } from "./delete-users.services";
 
@@ -22,6 +23,10 @@ jest.mock("../_shared/infra/cognito", () => ({
   },
 }));
 
+const request = {
+  id: "1",
+};
+
 describe("Delete Users Service", () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -32,8 +37,32 @@ describe("Delete Users Service", () => {
   });
 
   it("should be able to call findById", async () => {
-    await service({ id: "1" });
+    await service(request);
 
     expect(userRepository.findById).toHaveBeenCalledWith("1");
+  });
+
+  it("should throw an error if user is not found", async () => {
+    (userRepository.findById as jest.Mock).mockResolvedValueOnce(undefined);
+
+    const result = await service(request);
+
+    expect(result).toEqual({
+      message: "User not found",
+      success: false,
+      data: null,
+    });
+  });
+
+  it("should be able to call removeUser", async () => {
+    await service(request);
+
+    expect(cognito.removeUser).toHaveBeenCalledWith("user1@example.com");
+  });
+
+  it("should be able to call delete", async () => {
+    await service(request);
+
+    expect(userRepository.delete).toHaveBeenCalledWith("user1@example.com");
   });
 });
