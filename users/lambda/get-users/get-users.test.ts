6985690f1fa961f783d42a3644471508b21fa083
igorcruzz <igorcruz.dev@gmail.com>
2025-08-08 20:49:00 -0300

    test: add unit tests for Get Users Service to verify getUsers functionality

--- users/lambda/get-users/get-users.test.ts
+++ users/lambda/get-users/get-users.test.ts
@@ -1,4 +1,19 @@
 import { service } from "./get-users.services";
+import { userRepository } from "../_shared/repository/user.repository";
+
+jest.mock("../_shared/repository/user.repository", () => ({
+  userRepository: {
+    getUsers: jest.fn().mockResolvedValue([
+      {
+        id: "1",
+        name: "User One",
+        email: "user1@example.com",
+        password: "hashedpassword",
+        createdAt: new Date(),
+      },
+    ]),
+  },
+}));
 
 describe("Get Users Service", () => {
   beforeEach(() => {
@@ -7,5 +22,17 @@ describe("Get Users Service", () => {
 
   it("should be defined", async () => {
     expect(service).toBeDefined();
+  });
+
+  it("should be able to call getUsers", async () => {
+    await service({
+      limit: 10,
+      page: 1,
+    });
+
+    expect(userRepository.getUsers).toHaveBeenCalledWith({
+      limit: 10,
+      page: 1,
+    });
   });
 });
