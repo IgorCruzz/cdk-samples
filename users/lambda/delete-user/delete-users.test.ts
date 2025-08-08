6226b3e42c4530ae41c807706ecd2727e3dc86a6
igorcruzz <igorcruz.dev@gmail.com>
2025-08-07 23:11:37 -0300

    test: add unit tests for Delete Users Service to verify findById functionality

--- users/lambda/delete-user/delete-users.test.ts
+++ users/lambda/delete-user/delete-users.test.ts
@@ -1,4 +1,26 @@
+import { userRepository } from "../_shared/repository/user.repository";
 import { service } from "./delete-users.services";
+
+jest.mock("../_shared/repository/user.repository", () => ({
+  userRepository: {
+    delete: jest.fn(),
+    findById: jest.fn().mockResolvedValue([
+      {
+        id: "1",
+        name: "User One",
+        email: "user1@example.com",
+        password: "hashedpassword",
+        createdAt: new Date(),
+      },
+    ]),
+  },
+}));
+
+jest.mock("../_shared/infra/cognito", () => ({
+  cognito: {
+    removeUser: jest.fn(),
+  },
+}));
 
 describe("Delete Users Service", () => {
   beforeEach(() => {
@@ -7,5 +29,11 @@ describe("Delete Users Service", () => {
 
   it("should be defined", async () => {
     expect(service).toBeDefined();
+  });
+
+  it("should be able to call findById", async () => {
+    await service({ id: "1" });
+
+    expect(userRepository.findById).toHaveBeenCalledWith("1");
   });
 });
