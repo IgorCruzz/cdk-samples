363f4964fb28a4790094db09184c7ad4bfb952f0
igorcruzz <igorcruz.dev@gmail.com>
2025-08-09 22:20:47 -0300

    test: add unit tests for extract-data, generate-presigned-url, get-data, and get-files services

---
+++ csv-parse/lambda/generate-presigned-url/generate-presigned-url.test.ts
@@ -0,0 +1,117 @@
+import { service } from "../generate-presigned-url/generate-presigned-url.service";
+import { archiveRepository } from "../_shared/repository/archive.repository";
+import { userRepository } from "../_shared/repository/user.repository";
+import { s3 } from "../_shared/infra/s3";
+
+jest.mock("../../_shared/infra/s3", () => ({
+  s3: {
+    createPresignedUrl: jest.fn().mockResolvedValue({
+      url: "https://fake-s3-url.com/upload.csv",
+      key: "mocked-key.csv",
+    }),
+    getObject: jest.fn().mockResolvedValue({
+      pipe: jest.fn(),
+    }),
+    removeObject: jest.fn().mockResolvedValue(undefined),
+  },
+}));
+
+jest.mock("../../_shared/repository/archive.repository", () => ({
+  archiveRepository: {
+    getFileByKey: jest.fn().mockResolvedValue({
+      key: "key",
+      size: 0,
+      message: "message",
+      status: "PROCESSING",
+      lines: 0,
+      userId: "userId",
+      id: "id",
+      filename: "filename",
+      user: {
+        email: "email@mail.com",
+      },
+    }),
+    updateStatus: jest.fn(),
+    save: jest.fn(),
+  },
+}));
+
+jest.mock("../../_shared/repository/user.repository", () => ({
+  userRepository: {
+    findBySub: jest.fn().mockResolvedValue({
+      id: "userId",
+      email: "email@mail.com",
+      providers: {
+        google: "googleId",
+        cognito: "cognitoId",
+      },
+    }),
+  },
+}));
+
+describe("generatePresignedUrl", () => {
+  it("should be defined", async () => {
+    expect(service).toBeDefined();
+  });
+
+  it("should be able to call createPresignedUrl", async () => {
+    await service({
+      userId: "userId",
+      size: 123465,
+      filename: "filename",
+    });
+
+    expect(s3.createPresignedUrl).toHaveBeenCalled();
+  });
+
+  it("should be able to call findBySub", async () => {
+    await service({
+      userId: "userId",
+      size: 123465,
+      filename: "filename",
+    });
+
+    expect(userRepository.findBySub).toHaveBeenCalledWith("userId");
+  });
+
+  it("throw an error if findBySub throws", async () => {
+    (userRepository.findBySub as jest.Mock).mockResolvedValueOnce(null);
+    const svc = service({
+      userId: "userId",
+      size: 123465,
+      filename: "filename",
+    });
+
+    expect(svc).rejects.toThrow("User not found");
+  });
+
+  it("should be able to call save", async () => {
+    await service({
+      userId: "userId",
+      size: 123465,
+      filename: "filename",
+    });
+
+    expect(archiveRepository.save).toHaveBeenCalledWith({
+      key: "mocked-key.csv",
+      size: 123465,
+      message: ``,
+      status: "PENDING",
+      userId: "userId",
+      filename: "filename",
+    });
+  });
+
+  it("should return url and key on success", async () => {
+    const svc = await service({
+      userId: "userId",
+      size: 123465,
+      filename: "filename",
+    });
+
+    expect(svc).toEqual({
+      url: "https://fake-s3-url.com/upload.csv",
+      key: "mocked-key.csv",
+    });
+  });
+});
---
+++ csv-parse/lambda/get-data/get-data.test.ts
@@ -0,0 +1,50 @@
+import { service } from "../get-data/get-data.services";
+import { dataRepository } from "../_shared/repository/data.repository";
+
+jest.mock("../../_shared/repository/data.repository", () => ({
+  dataRepository: {
+    get: jest.fn().mockResolvedValue({
+      itens: [{ id: "mocked-id", name: "mocked-name" }],
+      count: 1,
+      page: 1,
+      limit: 10,
+      totalPages: 1,
+    }),
+  },
+}));
+
+describe("getData", () => {
+  it("should be defined", async () => {
+    expect(service).toBeDefined();
+  });
+
+  it("should be able to call get data", async () => {
+    await service({
+      archiveId: "id",
+      limit: 10,
+      page: 1,
+    });
+
+    expect(dataRepository.get).toHaveBeenCalledWith({
+      archiveId: "id",
+      limit: 10,
+      page: 1,
+    });
+  });
+
+  it("should return data on success", async () => {
+    const svc = await service({
+      archiveId: "id",
+      limit: 10,
+      page: 1,
+    });
+
+    expect(svc).toEqual({
+      count: 1,
+      itens: [{ id: "mocked-id", name: "mocked-name" }],
+      limit: 10,
+      page: 1,
+      totalPages: 1,
+    });
+  });
+});
