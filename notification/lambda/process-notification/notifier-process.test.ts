70f3d16d429f25d640abed48ff96631657d5224b
igorcruzz <igorcruz.dev@gmail.com>
2025-08-09 22:30:48 -0300

    fix: update import paths for mail and twilio mocks in notifier-process test file

--- notification/lambda/process-notification/notifier-process.test.ts
+++ notification/lambda/process-notification/notifier-process.test.ts
@@ -2,13 +2,13 @@ import { service } from "../process-notification/notifier-process.service";
 import { mail } from "../_shared/infra/mail";
 import { twilio } from "../_shared/infra/twilio";
 
-jest.mock("../../_shared/infra/mail", () => ({
+jest.mock("../_shared/infra/mail", () => ({
   mail: {
     sendMail: jest.fn().mockResolvedValue(undefined),
   },
 }));
 
-jest.mock("../../_shared/infra/twilio", () => ({
+jest.mock("../_shared/infra/twilio", () => ({
   twilio: {
     sendWhatsAppMessage: jest.fn().mockResolvedValue(undefined),
   },
