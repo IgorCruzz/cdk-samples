287bdd8f5c3b7693af648dc55f5ecc3cf173f883
igorcruzz <igorcruz.dev@gmail.com>
2025-08-09 22:31:19 -0300

    chore: remove console log from handler function

--- notification/lambda/process-notification/handler.ts
+++ notification/lambda/process-notification/handler.ts
@@ -4,7 +4,5 @@ import { service } from "./notifier-process.service";
 export const handler = async (event: SQSEvent) => {
   const { Records } = event;
 
-  console.log({ event });
-
   return await service({ records: Records });
 };
