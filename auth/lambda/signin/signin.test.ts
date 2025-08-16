0343036044fa8078297f720898190b16a5d78681
igorcruzz <igorcruz.dev@gmail.com>
2025-08-15 23:04:04 -0300

    fix: add user retrieval and validation in signin tests

--- auth/lambda/signin/signin.test.ts
+++ auth/lambda/signin/signin.test.ts
@@ -1,5 +1,6 @@
 import { service } from "../signin/signin.services";
 import { cognito } from "../_shared/infra/cognito";
+import { userRepository } from "../_shared/repository/user.repository";
 
 jest.mock("../_shared/infra/cognito", () => ({
   cognito: {
@@ -8,6 +9,19 @@ jest.mock("../_shared/infra/cognito", () => ({
       refreshToken: "refreshToken",
       idToken: "idToken",
     }),
+  },
+}));
+
+jest.mock("../_shared/repository/user.repository", () => ({
+  userRepository: {
+    findByEmail: jest.fn().mockResolvedValue({
+      id: "1",
+      name: "User One",
+      email: "user1@example.com",
+      password: "hashedpassword",
+      createdAt: new Date(),
+    }),
+    save: jest.fn(),
   },
 }));
 
@@ -46,6 +60,32 @@ describe("signin services", () => {
     });
   });
 
+  it("should be able to call findByEmail", async () => {
+    await service({
+      email: "test@example.com",
+      password: "password123",
+    });
+
+    expect(userRepository.findByEmail).toHaveBeenCalledWith({
+      email: "test@example.com",
+    });
+  });
+
+  it("should be able to return sucess false if user not exists", async () => {
+    (userRepository.findByEmail as jest.Mock).mockResolvedValueOnce(null);
+
+    const response = await service({
+      email: "test@example.com",
+      password: "password123",
+    });
+
+    expect(response).toEqual({
+      message: "Invalid email or password",
+      success: false,
+      data: null,
+    });
+  });
+
   it("should return success when auth succeeds", async () => {
     const response = await service({
       email: "test@example.com",
@@ -59,6 +99,9 @@ describe("signin services", () => {
         accessToken: "accessToken",
         refreshToken: "refreshToken",
         idToken: "idToken",
+        user: {
+          name: "User One",
+        },
       },
     });
     expect(cognito.auth).toHaveBeenCalledTimes(1);
