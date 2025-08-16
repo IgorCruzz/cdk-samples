2df07e5ed86086e92d05608604186b5e3198d5cf
igorcruzz <igorcruz.dev@gmail.com>
2025-08-16 14:09:55 -0300

    fix: remove unused SidebarTrigger import from AppLayout component

--- frontend/app/src/components/AppLayout.tsx
+++ frontend/app/src/components/AppLayout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { AppSidebar } from './Sidebar';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
 const AppLayout = () => {
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <SidebarProvider>
         <AppSidebar />
-        <SidebarTrigger />
+
         <main className="flex-1 p-5">
           <Outlet />
         </main>
