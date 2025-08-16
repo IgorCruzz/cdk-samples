71e88acf5bb37a49dc6d635b6448451f52d59178
igorcruzz <igorcruz.dev@gmail.com>
2025-08-15 23:58:53 -0300

    fix: establish database connection in signin handler

--- auth/lambda/signin/handler.ts
+++ auth/lambda/signin/handler.ts
@@ -1,9 +1,19 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { service } from "./signin.services";
 import { internal } from "../_shared/http/500";
+import { secret } from "../_shared/infra/secret";
+import { dbHelper } from "../_shared/repository/db-helper";
+
+let isConnected = false;
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   try {
+    if (!isConnected) {
+      const uri = await secret.getMongoUri();
+      await dbHelper.connect(uri);
+      isConnected = true;
+    }
+
     const data = JSON.parse(event.body || "{}");
 
     const response = await service(data);
