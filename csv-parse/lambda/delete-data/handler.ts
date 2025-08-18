27420ad55cce4a702f0504d95e0582a56a4a09ce
igorcruzz <igorcruz.dev@gmail.com>
2025-08-18 20:40:16 -0300

    feat: implement handler for deleting data with pagination support

---
+++ csv-parse/lambda/delete-data/handler.ts
@@ -0,0 +1,36 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { service } from "./get-data.services";
+
+import { dbHelper } from "../_shared/repository/db-helper";
+import { secret } from "../_shared/infra/secret";
+
+let isConnected = false;
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    if (!isConnected) {
+      const uri = await secret.getMongoUri();
+      await dbHelper.connect(uri);
+      isConnected = true;
+    }
+
+    const page = parseInt(event.queryStringParameters?.page || "1");
+
+    const limit = parseInt(event.queryStringParameters?.limit || "20");
+
+    const { archiveId } = event.pathParameters as { archiveId: string };
+
+    const files = await service({ page, limit, archiveId });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(files),
+    };
+  } catch (error) {
+    console.error("Error in handler:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
+};
