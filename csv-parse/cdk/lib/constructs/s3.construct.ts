dab81734c1401388d88b9725d598f3aa09be2f62
igorcruzz <igorcruz.dev@gmail.com>
2025-08-17 21:21:25 -0300

    fix: remove unused createS3BucketData method for cleaner implementation

--- csv-parse/cdk/lib/constructs/s3.construct.ts
+++ csv-parse/cdk/lib/constructs/s3.construct.ts
@@ -9,7 +9,6 @@ export class S3Construct extends Construct {
     super(scope, id);
 
     this.bucket = this.createS3Bucket();
-    this.createS3BucketData();
   }
 
   createS3Bucket() {
@@ -23,12 +22,6 @@ export class S3Construct extends Construct {
           allowedHeaders: ["*"],
         },
       ],
-    });
-  }
-
-  createS3BucketData() {
-    return new Bucket(this, "bucket-csv-parse-data", {
-      removalPolicy: RemovalPolicy.RETAIN,
     });
   }
 }
