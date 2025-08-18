cf05b5d68e6c4d0fe90a3b980425b526329a53ad
igorcruzz <igorcruz.dev@gmail.com>
2025-08-18 20:46:48 -0300

    test: update delete-data test to correctly mock dataRepository.delete and verify call

--- csv-parse/lambda/delete-data/delete-data.test.ts
+++ csv-parse/lambda/delete-data/delete-data.test.ts
@@ -1,8 +1,20 @@
-import { service } from "../get-data/get-data.services";
+import { service } from "../delete-data/delete-data.services";
 import { dataRepository } from "../_shared/repository/data.repository";
+
+jest.mock("../_shared/repository/data.repository", () => ({
+  dataRepository: {
+    delete: jest.fn().mockResolvedValue(undefined),
+  },
+}));
 
 describe("deleteData", () => {
   it("should be defined", async () => {
     expect(service).toBeDefined();
   });
+
+  it("should be able to call delete", async () => {
+    await service({ id: "awesome-id" });
+  });
+
+  expect(dataRepository.delete).toHaveBeenCalledWith({ id: "awesome-id" });
 });
