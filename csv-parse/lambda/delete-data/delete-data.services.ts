c88180a567b222285da605a2c5ef0ef98faced6b
igorcruzz <igorcruz.dev@gmail.com>
2025-08-18 20:46:45 -0300

    fix: ensure delete method in delete-data service correctly calls dataRepository.delete

--- csv-parse/lambda/delete-data/delete-data.services.ts
+++ csv-parse/lambda/delete-data/delete-data.services.ts
@@ -1,5 +1,7 @@
 import { dataRepository } from "../_shared/repository/data.repository";
 
 export const service = async ({ id }: { id: string }) => {
+  await dataRepository.delete({ id });
+
   return null;
 };
