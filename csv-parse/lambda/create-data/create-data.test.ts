6050af8c0a5401e7ff0795a2113517e23057441a
igorcruzz <igorcruz.dev@gmail.com>
2025-08-18 22:16:50 -0300

    test: add unit test for createData service and mock dataRepository

--- csv-parse/lambda/create-data/create-data.test.ts
+++ csv-parse/lambda/create-data/create-data.test.ts
@@ -1,8 +1,40 @@
-import { service } from "../delete-data/delete-data.services";
+import { service } from "../create-data/create-data.services";
 import { dataRepository } from "../_shared/repository/data.repository";
+
+jest.mock("../_shared/repository/data.repository", () => ({
+  dataRepository: {
+    singleSave: jest.fn(),
+  },
+}));
 
 describe("createData", () => {
   it("should be defined", async () => {
     expect(service).toBeDefined();
+  });
+
+  it("should be able to call singleSave", async () => {
+    await service({
+      nome: "Teixeira Cardoso - ME",
+      cnpj: "23.579.041/0001-80",
+      email: "rodrigueslivia@azevedo.com",
+      telefone: "(40) 6977-4635",
+      endereco: "Jardim de Mendes, 18",
+      cidade: "Belém",
+      estado: "MT",
+      cep: "50588-358",
+      archiveId: "68a3b89d64b9a8037c97582d",
+    });
+
+    expect(dataRepository.singleSave).toHaveBeenCalledWith({
+      nome: "Teixeira Cardoso - ME",
+      cnpj: "23.579.041/0001-80",
+      email: "rodrigueslivia@azevedo.com",
+      telefone: "(40) 6977-4635",
+      endereco: "Jardim de Mendes, 18",
+      cidade: "Belém",
+      estado: "MT",
+      cep: "50588-358",
+      archiveId: "68a3b89d64b9a8037c97582d",
+    });
   });
 });
