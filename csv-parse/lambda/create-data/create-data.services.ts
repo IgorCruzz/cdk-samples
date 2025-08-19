8f62623a97f23f0c85e2fa6bd27152407796035a
igorcruzz <igorcruz.dev@gmail.com>
2025-08-18 22:24:43 -0300

    fix: update service method input type to Record<string, unknown>

--- csv-parse/lambda/create-data/create-data.services.ts
+++ csv-parse/lambda/create-data/create-data.services.ts
@@ -1,6 +1,6 @@
 import { dataRepository } from "../_shared/repository/data.repository";
 
-export const service = async (data: unknown) => {
+export const service = async (data: Record<string, unknown>) => {
   await dataRepository.singleSave(data);
 
   return { message: "Data created successfully", success: true, data: null };
