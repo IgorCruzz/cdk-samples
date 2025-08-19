1447ce597945504053046c7a5d550a0434e20df4
igorcruzz <igorcruz.dev@gmail.com>
2025-08-18 22:40:30 -0300

    feat: add handler for updating data with MongoDB connection and response handling

---
+++ csv-parse/lambda/update-data/handler.ts
@@ -0,0 +1,43 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { service } from "./update-data.services";
+
+import { dbHelper } from "../_shared/repository/db-helper";
+import { secret } from "../_shared/infra/secret";
+
+let isConnected = false;
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    if (!isConnected) {
+      const uri = await secret.getMongoUri();
+      await dbHelper.connect(uri);
+      isConnected = true;
+    }
+
+    const { archiveId } = event.pathParameters as {
+      archiveId: string;
+    };
+
+    const body = JSON.parse(event.body || "{}");
+
+    const response = await service({ ...body, archiveId });
+
+    if (!response.success) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(response),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    console.error("Error in handler:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
+};
