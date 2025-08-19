fbb712e1f8273a4b4244243b0945feec640f4372
igorcruzz <igorcruz.dev@gmail.com>
2025-08-18 22:49:56 -0300

    feat: update service function to include id parameter for data updates

--- csv-parse/lambda/update-data/update-data.services.ts
+++ csv-parse/lambda/update-data/update-data.services.ts
@@ -1,5 +1,7 @@
 import { dataRepository } from "../_shared/repository/data.repository";
 
-export const service = async (data: Record<string, unknown>) => {
+export const service = async (data: Record<string, unknown>, id: string) => {
+  await dataRepository.updateData(data, id);
+
   return { message: "Data updated successfully", success: true, data: null };
 };
