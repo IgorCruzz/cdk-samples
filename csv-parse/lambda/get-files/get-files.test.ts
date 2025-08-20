18da2f8bfdba5729d728979f18933c33341969da
igorcruzz <igorcruz.dev@gmail.com>
2025-08-20 19:31:54 -0300

    refactor: update mock data in getFiles tests for improved accuracy

--- csv-parse/lambda/get-files/get-files.test.ts
+++ csv-parse/lambda/get-files/get-files.test.ts
@@ -1,6 +1,12 @@
 import { service } from "../get-files/get-files.services";
 import { archiveRepository } from "../_shared/repository/archive.repository";
 
+jest.mock("../_shared/repository/data.repository", () => ({
+  dataRepository: {
+    singleSave: jest.fn(),
+  },
+}));
+
 jest.mock("../_shared/repository/archive.repository", () => ({
   archiveRepository: {
     getFiles: jest.fn().mockResolvedValue({
@@ -8,11 +14,11 @@ jest.mock("../_shared/repository/archive.repository", () => ({
         {
           key: "file-key-1",
           size: 12345,
+          endpoint: "endpoint",
           message: "File processed",
           status: "COMPLETED",
-          lines: 100,
           userId: "user-id-1",
-          id: "id-1",
+          id: "68a3b89d64b9a8037c97582d",
           filename: "file1.csv",
           user: {
             email: "user1@example.com",
