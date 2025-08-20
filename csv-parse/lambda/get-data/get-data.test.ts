16fe164c5ea3963c601827feeef394c21d8769db
igorcruzz <igorcruz.dev@gmail.com>
2025-08-20 19:31:50 -0300

    refactor: enhance archive repository mock in getData tests for improved accuracy

--- csv-parse/lambda/get-data/get-data.test.ts
+++ csv-parse/lambda/get-data/get-data.test.ts
@@ -13,6 +13,21 @@ jest.mock("../_shared/repository/data.repository", () => ({
   },
 }));
 
+jest.mock("../_shared/repository/archive.repository", () => ({
+  archiveRepository: {
+    getByEndpoint: jest.fn().mockResolvedValue({
+      key: "file-key-1",
+      size: 12345,
+      endpoint: "endpoint",
+      message: "File processed",
+      status: "COMPLETED",
+      userId: "user-id-1",
+      id: "68a3b89d64b9a8037c97582d",
+      filename: "file1.csv",
+    }),
+  },
+}));
+
 const request = {
   endpoint: "endpoint",
   userId: "userId",
@@ -29,7 +44,7 @@ describe("getData", () => {
     await service(request);
 
     expect(dataRepository.get).toHaveBeenCalledWith({
-      archiveId: "id",
+      archiveId: "68a3b89d64b9a8037c97582d",
       limit: 10,
       page: 1,
     });
