72f9ca7897bf5359e9cdad6d63e9885784f85d95
igorcruzz <igorcruz.dev@gmail.com>
2025-08-20 19:31:59 -0300

    refactor: mock archive repository in updateData tests for improved isolation

--- csv-parse/lambda/update-data/update-data.test.ts
+++ csv-parse/lambda/update-data/update-data.test.ts
@@ -7,6 +7,21 @@ jest.mock("../_shared/repository/data.repository", () => ({
   },
 }));
 
+jest.mock("../_shared/repository/archive.repository", () => ({
+  archiveRepository: {
+    getByEndpoint: jest.fn().mockResolvedValue({
+      key: "file-key-1",
+      size: 12345,
+      endpoint: "endpoint",
+      message: "File processed",
+      status: "COMPLETED",
+      userId: "user-id-1",
+      id: "68a3b89d64b9a8037c97582d",
+      filename: "file1.csv",
+    }),
+  },
+}));
+
 const request = {
   nome: "Teixeira Cardoso - ME",
   cnpj: "23.579.041/0001-80",
