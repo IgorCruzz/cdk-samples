3edbf15a5b6b5324085a538f0e8092dce82af07c
igorcruzz <igorcruz.dev@gmail.com>
2025-08-20 20:15:15 -0300

    refactor: restructure service parameters for improved clarity

--- csv-parse/lambda/update-data/update-data.services.ts
+++ csv-parse/lambda/update-data/update-data.services.ts
@@ -1,6 +1,11 @@
 import { dataRepository } from "../_shared/repository/data.repository";
 
-export const service = async (data: Record<string, unknown>, id: string) => {
+type Input = {
+  data: Record<string, unknown>;
+  id: string;
+};
+
+export const service = async ({ data, id }: Input) => {
   await dataRepository.updateData(data, id);
 
   return { message: "Data updated successfully", success: true, data: null };
