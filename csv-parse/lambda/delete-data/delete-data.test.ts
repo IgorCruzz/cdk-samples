e3dc36630125ea71e4ac0f666e51eba4a323260d
igorcruzz <igorcruz.dev@gmail.com>
2025-08-20 19:31:42 -0300

    refactor: mock archive repository in deleteData tests for improved isolation

--- csv-parse/lambda/delete-data/delete-data.test.ts
+++ csv-parse/lambda/delete-data/delete-data.test.ts
@@ -11,6 +11,21 @@ jest.mock("../_shared/repository/data.repository", () => ({
   },
 }));
 
+jest.mock("../_shared/repository/archive.repository", () => ({
+  archiveRepository: {
+    getByEndpoint: jest.fn().mockResolvedValue({
+      key: "file-key-1",
+      size: 12345,
+      endpoint: "endpoint",
+      message: "File processed",
+      status: "COMPLETED",
+      userId: "user-id-1",
+      id: "68a3b89d64b9a8037c97582d",
+      filename: "file1.csv",
+    }),
+  },
+}));
+
 const request = { id: "awesome-id", endpoint: "endpoint", userId: "userId" };
 
 describe("deleteData", () => {
