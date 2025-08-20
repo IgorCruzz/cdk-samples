ba5a64ebf68a714db93e4edfd596668e671566ca
igorcruzz <igorcruz.dev@gmail.com>
2025-08-19 21:07:05 -0300

    feat: refactor service to retrieve archive by endpoint and userId, improving data fetching logic

--- csv-parse/lambda/get-data/get-data.services.ts
+++ csv-parse/lambda/get-data/get-data.services.ts
@@ -1,13 +1,16 @@
 import { dataRepository } from "../_shared/repository/data.repository";
+import { archiveRepository } from "../_shared/repository/archive.repository";
 
 export const service = async ({
   page,
   limit,
-  archiveId,
+  userId,
+  endpoint,
 }: {
   page: number;
   limit: number;
-  archiveId: string;
+  userId: string;
+  endpoint: string;
 }): Promise<{
   itens: unknown[];
   count: number;
@@ -15,10 +18,25 @@ export const service = async ({
   limit: number;
   totalPages: number;
 }> => {
+  const archive = await archiveRepository.getByEndpoint({
+    endpoint,
+    userId,
+  });
+
+  if (!archive) {
+    return {
+      itens: [],
+      count: 0,
+      page,
+      limit,
+      totalPages: 0,
+    };
+  }
+
   const data = await dataRepository.get({
     page,
     limit,
-    archiveId,
+    archiveId: archive.id!,
   });
 
   return data;
