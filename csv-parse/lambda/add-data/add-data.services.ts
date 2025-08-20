f4b9b9391a3aff77c4abaf268be87d449aa9c076
igorcruzz <igorcruz.dev@gmail.com>
2025-08-19 23:34:51 -0300

    feat: implement archive check before saving data to ensure data integrity

--- csv-parse/lambda/add-data/add-data.services.ts
+++ csv-parse/lambda/add-data/add-data.services.ts
@@ -1,7 +1,17 @@
+import { archiveRepository } from "../_shared/repository/archive.repository";
 import { dataRepository } from "../_shared/repository/data.repository";
 
 export const service = async (data: Record<string, unknown>) => {
-  await dataRepository.singleSave(data);
+  const archive = await archiveRepository.getByEndpoint({
+    endpoint: data.endpoint as string,
+    userId: data.userId as string,
+  });
+
+  if (!archive) {
+    return { message: "Data not found", success: false, data: null };
+  }
+
+  await dataRepository.singleSave({ ...data, archiveId: archive.id });
 
   return { message: "Data created successfully", success: true, data: null };
 };
