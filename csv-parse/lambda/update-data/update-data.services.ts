c025d93b203a56b66d0b22f9c6af16a2e812e777
igorcruzz <igorcruz.dev@gmail.com>
2025-08-20 20:54:37 -0300

    refactor: update service method to destructure parameters and improve error handling

--- csv-parse/lambda/update-data/update-data.services.ts
+++ csv-parse/lambda/update-data/update-data.services.ts
@@ -1,12 +1,24 @@
+import { archiveRepository } from "../_shared/repository/archive.repository";
 import { dataRepository } from "../_shared/repository/data.repository";
 
 type Input = {
   data: Record<string, unknown>;
-  id: string;
+  userId: string;
+  endpoint: string;
+  dataId: string;
 };
 
-export const service = async ({ data, id }: Input) => {
-  await dataRepository.updateData(data, id);
+export const service = async ({ data, endpoint, userId, dataId }: Input) => {
+  const archive = await archiveRepository.getByEndpoint({
+    endpoint,
+    userId,
+  });
+
+  if (!archive) {
+    return { message: "Api not found", success: false, data: null };
+  }
+
+  await dataRepository.updateData({ data, id: dataId });
 
   return { message: "Data updated successfully", success: true, data: null };
 };
