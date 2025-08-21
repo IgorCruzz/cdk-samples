76292d64d390640fcf65d0b46928d71d4e36593a
igorcruzz <igorcruz.dev@gmail.com>
2025-08-20 21:21:16 -0300

    feat: implement handler for delete API to process archive deletion requests

---
+++ csv-parse/lambda/delete-api/handler.ts
@@ -0,0 +1,40 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { service } from "./delete-api.services";
+
+import { dbHelper } from "../_shared/repository/db-helper";
+import { secret } from "../_shared/infra/secret";
+
+let isConnected = false;
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    if (!isConnected) {
+      const uri = await secret.getMongoUri();
+      await dbHelper.connect(uri);
+      isConnected = true;
+    }
+    const { archiveId } = event.pathParameters as {
+      archiveId: string;
+    };
+
+    const response = await service({ archiveId });
+
+    if (!response.success) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(response),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    console.error("Error in handler:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
+};
