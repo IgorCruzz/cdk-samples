3bdecc1817fd6b915dd86003c0c35f4c1dfaf92f
igorcruzz <igorcruz.dev@gmail.com>
2025-08-20 21:29:22 -0300

    test: add delete method mock and verify delete call in deleteData tests

--- csv-parse/lambda/delete-api/delete-data.test.ts
+++ csv-parse/lambda/delete-api/delete-data.test.ts
@@ -3,6 +3,7 @@ import { archiveRepository } from "../_shared/repository/archive.repository";
 
 jest.mock("../_shared/repository/archive.repository", () => ({
   archiveRepository: {
+    delete: jest.fn().mockResolvedValue(undefined),
     getFileById: jest.fn().mockResolvedValue({
       key: "key",
       size: 0,
@@ -34,12 +35,22 @@ describe("deleteData", () => {
   });
 
   it("should return success false when archive does not exists", async () => {
+    (archiveRepository.getFileById as jest.Mock).mockResolvedValueOnce(null);
+
     const svc = await service(request);
 
     expect(svc).toEqual({
       message: "Api does not exists",
       success: false,
       data: null,
+    });
+  });
+
+  it("should be able to call delete", async () => {
+    await service(request);
+
+    expect(archiveRepository.delete).toHaveBeenCalledWith({
+      id: "some-archive-id",
     });
   });
 });
