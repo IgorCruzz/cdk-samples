5fb4da8a2748e587c1646e167adbdd479d8e85e7
igorcruzz <igorcruz.dev@gmail.com>
2025-08-20 21:21:01 -0300

    test: add unit tests for deleteData service functionality

---
+++ csv-parse/lambda/delete-api/delete-data.test.ts
@@ -0,0 +1,36 @@
+import { service } from "../delete-api/delete-api.services";
+import { archiveRepository } from "../_shared/repository/archive.repository";
+
+jest.mock("../_shared/repository/archive.repository", () => ({
+  archiveRepository: {
+    getFileById: jest.fn(),
+  },
+}));
+
+const request = {
+  archiveId: "some-archive-id",
+};
+
+describe("deleteData", () => {
+  it("should be defined", async () => {
+    expect(service).toBeDefined();
+  });
+
+  it("should be able to call getFileById", async () => {
+    await service(request);
+
+    expect(archiveRepository.getFileById).toHaveBeenCalledWith({
+      id: "some-archive-id",
+    });
+  });
+
+  it("should return success false when archive does not exists", async () => {
+    const svc = await service(request);
+
+    expect(svc).toEqual({
+      message: "Api does not exists",
+      success: false,
+      data: null,
+    });
+  });
+});
