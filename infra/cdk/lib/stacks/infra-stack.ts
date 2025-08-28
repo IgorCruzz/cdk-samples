3551d52ba66849973c982aa009dc1bc13f8eba7b
igorcruzz <igorcruz.dev@gmail.com>
2025-08-28 20:31:15 -0300

    feat: add VPC construct instantiation in InfraStack

--- infra/cdk/lib/stacks/infra-stack.ts
+++ infra/cdk/lib/stacks/infra-stack.ts
@@ -4,11 +4,14 @@ import {
   CertificateConstruct,
   ApiConstruct,
   CognitoConstruct,
+  VpcConstruct,
 } from "../constructs";
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    new VpcConstruct(this, "construct-vpc");
 
     const { certificate } = new CertificateConstruct(
       this,
