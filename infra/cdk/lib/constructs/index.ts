c3f66c05f2f0f2b4d2a95f63c2f7b75e4a37ac1c
igorcruzz <igorcruz.dev@gmail.com>
2025-08-28 20:31:42 -0300

    feat: export VpcConstruct from vpc.construct in index.ts

--- infra/cdk/lib/constructs/index.ts
+++ infra/cdk/lib/constructs/index.ts
@@ -1,3 +1,4 @@
 export { CertificateConstruct } from "./certificate.construct";
 export { ApiConstruct } from "./api.construct";
 export { CognitoConstruct } from "./cognito.construct";
+export { VpcConstruct } from "./vpc.construct";
