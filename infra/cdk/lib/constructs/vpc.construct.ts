156a147bbaec6c2e5af92dfdf5e2dd5da4671218
igorcruzz <igorcruz.dev@gmail.com>
2025-09-04 23:42:07 -0300

    feat: refactor VPC creation logic for improved clarity and maintainability

--- infra/cdk/lib/constructs/vpc.construct.ts
+++ infra/cdk/lib/constructs/vpc.construct.ts
@@ -1,4 +1,5 @@
 import { Vpc } from "aws-cdk-lib/aws-ec2";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
 import { Construct } from "constructs";
 
 export class VpcConstruct extends Construct {
@@ -9,10 +10,29 @@ export class VpcConstruct extends Construct {
   }
 
   private createVPC() {
-    new Vpc(this, "vpc-1", {
+    const vpc = new Vpc(this, "vpc-1", {
       cidr: "10.0.0.0/16",
       maxAzs: 2,
       natGateways: 0,
+    });
+
+    new StringParameter(this, "vpc-id-parameter", {
+      parameterName: "/vpc/id",
+      stringValue: vpc.vpcId,
+    });
+
+    vpc.privateSubnets.forEach((subnet, index) => {
+      new StringParameter(this, `private-subnet-${index + 1}`, {
+        parameterName: `/vpc/private-subnet-${index + 1}`,
+        stringValue: subnet.subnetId,
+      });
+    });
+
+    vpc.publicSubnets.forEach((subnet, index) => {
+      new StringParameter(this, `public-subnet-${index + 1}`, {
+        parameterName: `/vpc/public-subnet-${index + 1}`,
+        stringValue: subnet.subnetId,
+      });
     });
   }
 }
