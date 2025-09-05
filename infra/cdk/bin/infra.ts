36e2ac0f5102f79d318ce7bc7df94187413ea11c
igorcruzz <igorcruz.dev@gmail.com>
2025-09-04 23:18:17 -0300

    feat: instantiate VPC stack in the main application entry point

--- infra/cdk/bin/infra.ts
+++ infra/cdk/bin/infra.ts
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { InfraStack } from "../lib/stacks/infra-stack";
+import { VpcStack } from "../lib/stacks/vpc-stack";
 
 const app = new cdk.App();
+
+new VpcStack(app, "stack-vpc", {});
+
 new InfraStack(app, "stack-infra", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
