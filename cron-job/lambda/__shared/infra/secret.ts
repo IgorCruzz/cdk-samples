8c2cfd9446f3d898b7eda067d92d47462941ed40
igorcruzz <igorcruz.dev@gmail.com>
2025-09-13 14:59:21 -0300

    feat: refactor database client to retrieve RDS credentials from secret management

--- cron-job/lambda/__shared/infra/secret.ts
+++ cron-job/lambda/__shared/infra/secret.ts
@@ -6,6 +6,28 @@ import {
 const secretsManager = new SecretsManagerClient({});
 
 export const secret = {
+  async getRdsCredentials(): Promise<{
+    username: string;
+    password: string;
+    host: string;
+    port: number;
+    dbname: string;
+  }> {
+    const res = await secretsManager.send(
+      new GetSecretValueCommand({
+        SecretId: process.env.RDS_SECRET_ARN,
+      })
+    );
+
+    const secret = JSON.parse(res.SecretString ?? "{}");
+    return {
+      username: secret["username"],
+      password: secret["password"],
+      host: secret["host"],
+      port: parseInt(secret["port"], 10),
+      dbname: secret["dbname"],
+    };
+  },
   async getBrevoApiKey(): Promise<string> {
     const res = await secretsManager.send(
       new GetSecretValueCommand({
--- cron-job/lambda/__shared/repository/client.ts
+++ cron-job/lambda/__shared/repository/client.ts
@@ -1,40 +1,21 @@
 import { Client } from "pg";
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager";
+import { secret } from "../infra/secret";
 
 export const dbHelper = {
   client: null as Client | null,
   async connect(): Promise<void> {
     if (this.client) return;
 
-    const secretArn = process.env.RDS_SECRET_ARN as string;
-
-    const secretsClient = new SecretsManagerClient({});
-    const command = new GetSecretValueCommand({ SecretId: secretArn });
-    const response = await secretsClient.send(command);
-
-    if (!response.SecretString) {
-      throw new Error("Secret not found or empty");
-    }
-
-    const secret = JSON.parse(response.SecretString);
-
-    const config = {
-      host: secret.host,
-      port: Number(secret.port),
-      username: secret.username,
-      password: secret.password,
-      dbname: secret.dbname || secret.database,
-    };
+    const rds = await secret.getRdsCredentials();
 
     this.client = new Client({
-      host: config.host,
-      port: config.port,
-      user: config.username,
-      password: config.password,
-      database: config.dbname,
+      host: rds.host,
+      port: rds.port,
+      user: rds.username,
+      password: rds.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     });
 
     await this.client.connect();
