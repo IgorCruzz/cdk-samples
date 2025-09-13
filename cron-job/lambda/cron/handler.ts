b96288c7bcffafd8d3fff73b290468ab40d0301e
igorcruzz <igorcruz.dev@gmail.com>
2025-09-13 13:30:55 -0300

    feat: establish database connection in cron job handler

--- cron-job/lambda/cron/handler.ts
+++ cron-job/lambda/cron/handler.ts
@@ -1,7 +1,10 @@
+import { dbHelper } from "../__shared/repository/client";
 import { service } from "./cron.service";
 
 export const handler = async () => {
   try {
+    await dbHelper.connect();
+
     await service();
   } catch (error) {
     console.error(error);
