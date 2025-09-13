7cdf1f3db8a8cc447ff1a2083bde137cec7552a8
igorcruzz <igorcruz.dev@gmail.com>
2025-09-13 09:51:16 -0300

    feat: add logging for the start of expired drivers check in cron service

--- cron-job/lambda/cron/cron.service.ts
+++ cron-job/lambda/cron/cron.service.ts
@@ -1,6 +1,8 @@
 import { driverRepository } from "../__shared/repository/driver.repository";
 
 export const service = async () => {
+  console.log("Starting expired drivers check...");
+
   const expiredDrivers = await driverRepository.getExpiredDrivers();
 
   if (!expiredDrivers.length) {
